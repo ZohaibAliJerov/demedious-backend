942af0e2eab615012252cc965597a0b91cc61805
sam <shoaibur206@gmail.com>
2021-10-25 11:49:07 +0500

    removed the autoScroll function

--- services/florence.js
+++ services/florence.js
@@ -13,26 +13,6 @@ const florenceService = async () => {
   await page.waitForTimeout(3000);
 
   //scroll the page
-  await autoScroll(page);
 };
-
-async function autoScroll(page) {
-  await page.evaluate(async () => {
-    await new Promise((resolve, reject) => {
-      var totalHeight = 0;
-      var distance = 100;
-      var timer = setInterval(() => {
-        var scrollHeight = document.body.scrollHeight;
-        window.scrollBy(0, distance);
-        totalHeight += distance;
-
-        if (totalHeight >= scrollHeight) {
-          clearInterval(timer);
-          resolve();
-        }
-      }, 100);
-    });
-  });
-}
 
 export default florenceService;
