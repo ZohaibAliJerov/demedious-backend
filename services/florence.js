c9222ed37f6586df8c0a7a9016555c1004092353
sam <shoaibur206@gmail.com>
2021-10-25 19:57:10 +0500

    modified scrolling script

--- services/florence.js
+++ services/florence.js
@@ -4,25 +4,31 @@
 import puppeteer from "puppeteer";
 
 const florenceService = async () => {
-  const browser = await puppeteer.launch({ headless: false });
-  const page = await browser.newPage();
-  const url =
-    "https://www.florence-nightingale-krankenhaus.de/de/karriere/stellenausschreibungen.html?type=0%27a%3D0%27a%3D0%3Fref%3Dausbildungsatlas";
-  await page.goto(url, { timeout: 0 });
-  await page.waitForNavigation();
+  try {
+    const browser = await puppeteer.launch({ headless: false });
+    const page = await browser.newPage();
+    await page.setDefaultNavigationTimeout(0);
+    const url =
+      "https://www.florence-nightingale-krankenhaus.de/de/karriere/stellenausschreibungen.html?type=0%27a%3D0%27a%3D0%3Fref%3Dausbildungsatlas";
+    await page.goto(url, { timeout: 0 });
 
-  //scroll the page
-  const distance = 100;
-  const delay = 100;
-  const timer = setInterval(() => {
-    document.scrollingElement.scrollBy(0, distance);
-    if (
-      document.scrollingElement.scrollTop + window.innerHeight >=
-      document.scrollingElement.scrollHeight
-    ) {
-      clearInterval(timer);
-    }
-  }, delay);
+    //scroll the page
+    await page.evaluate(() => {
+      const distance = 100;
+      const delay = 100;
+      const timer = setInterval(() => {
+        document.scrollingElement.scrollBy(0, distance);
+        if (
+          document.scrollingElement.scrollTop + window.innerHeight >=
+          document.scrollingElement.scrollHeight
+        ) {
+          clearInterval(timer);
+        }
+      }, delay);
+    });
+  } catch (err) {
+    console.log(err);
+  }
 };
 
 export default florenceService;
