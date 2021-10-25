dca58fa4cfcc62a320331926e3ada5cebb261613
ZohaibAliJerov <zuhaib911alijerov@gmail.com>
2021-10-25 22:56:45 +0530

    extract job titles and job links

--- services/beethoven-klinik.js
+++ services/beethoven-klinik.js
@@ -1,1 +1,27 @@
-import puppeteer from 'puppeteer';
+import puppeteer from "puppeteer";
+
+(async () => {
+  const browser = await puppeteer.launch({ headless: false });
+  const page = await browser.newPage();
+  const url = "https://www.beethoven-klinik-koeln.de/?s=jobs";
+  await page.goto(url);
+
+  // extracting job titles from page
+  const jobTitles = await page.evaluate(() =>
+    Array.from(
+      document.querySelectorAll("header.entry-header h1.entry-title")
+    ).map((title) => title.innerText)
+  );
+  console.log(jobTitles);
+
+  // extracting job links from first page
+
+  const jobLinks = await page.evaluate(() =>
+    Array.from(
+      document.querySelectorAll("header.entry-header h1.entry-title a")
+    ).map((link) => link.href)
+  );
+  console.log(jobLinks);
+
+  await browser.close();
+})();
