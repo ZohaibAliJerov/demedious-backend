a08e2c4c447461de86c9806a04318067b19b2233
sam <shoaibur206@gmail.com>
2021-11-09 22:31:50 +0500

    added scroll method

--- services/katharina.js
+++ services/katharina.js
@@ -55,6 +55,21 @@ const katharina = async () => {
     console.log(error);
   }
 };
+async function scroll(page) {
+  await page.evaluate(() => {
+    const distance = 100;
+    const delay = 100;
+    const timer = setInterval(() => {
+      document.scrollingElement.scrollBy(0, distance);
+      if (
+        document.scrollingElement.scrollTop + window.innerHeight >=
+        document.scrollingElement.scrollHeight
+      ) {
+        clearInterval(timer);
+      }
+    }, delay);
+  });
+}
 
 (async () => {
   katharina();
