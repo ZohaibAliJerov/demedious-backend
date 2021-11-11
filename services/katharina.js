a9c31c83d28980f7c258c8038c1284aa43c4b87f
sam <shoaibur206@gmail.com>
2021-11-11 15:42:49 +0500

    fixed a bug

--- services/katharina.js
+++ services/katharina.js
@@ -10,52 +10,54 @@ const katharina = async () => {
     await page.goto(
       "https://karriere.katharina-kasper-gruppe.de/stellenangebote.html?filter%5Borg_einheit%5D=3"
     );
-    await page.waitForNavigation();
+
     await page.screenshot({ path: "katharina.png" });
     await scroll(page);
     //get all jobsLinks
     await page.waitForSelector(".joboffer_title_text > a");
     let allJobLinks = await page.evaluate(() => {
       let jobLinks = document.querySelectorAll(".joboffer_title_text > a");
-      return jobLinks ? jobLinks.map((link) => link.href) : [];
+      return jobLinks ? [...jobLinks].map((link) => link.href) : [];
     });
 
     //get all jobs
     let allJobs = [];
     for (let jobLink of allJobLinks) {
       await page.goto(jobLink, { waitUntil: "load", timeout: 0 });
-      await page.waitForNavigation();
+
       await page.waitForTimeout(1000);
       await scroll(page);
       await page.waitForTimeout(1000);
       let newJob = {};
       newJob.title = await page.evaluate(() => {
         let title = document.querySelectorAll("h1");
-        return title ? [...title][1] : "";
+        return title ? [...title][1].innerText : "";
       });
       await page.waitForSelector(".content_text");
       newJob.address = await page.evaluate(() => {
-        let address = document.querySelectorAll(".content_text");
+        let address = Array.from(document.querySelectorAll(".content_text"));
+        address = address[4].innerText;
         return address
-          ? toString([...address][4]).replace(/\+\d+\s+\d+\s\d+-\d+/, "")
-          : "";
+          .split("\n")
+          .join(",")
+          .replace(/\+\d+\s+\d+\s\d+-\d+|,,/g, " ");
       });
       newJob.cell = await page.evaluate(() => {
-        let cell = document.querySelectorAll(".content_text");
-        return cell ? toString([...cell][4]).match(/\+\d+\s+\d+\s\d+-\d+/) : "";
+        let cell = Array.from(document.querySelectorAll(".content_text"));
+        return cell ? cell[4].innerText.match(/\+\d+\s+\d+\s\d+-\d+/) : "";
       });
       newJob.email = null;
 
       newJob.applyLink = await page.evaluate(() => {
-        let applyLink = document.querySelector("#btn_online_application");
+        let applyLink = document.querySelector("#btn_online_application > a");
         return applyLink ? applyLink.href : "";
       });
 
       allJobs.push(newJob);
     }
 
-    await page.close();
-    await browser.close();
+    // await page.close();
+    // await browser.close();
     return allJobs;
   } catch (error) {
     console.log(error);
@@ -78,5 +80,6 @@ async function scroll(page) {
 }
 
 (async () => {
-  katharina();
+  let allJobs = await katharina();
+  console.log(allJobs);
 })();
