eb654b354efd30467a6ec45a907bcf08bdfce017
sam <shoaibur206@gmail.com>
2021-11-12 16:34:47 +0500

    added findAllJobs route

--- api/routes/job.routes.js
+++ api/routes/job.routes.js
@@ -2,3 +2,6 @@ import JobContrller from "../controllers/job.controllers.js";
 import express from "express";
 import jobControllers from "../controllers/job.controllers.js";
 const router = express.Router();
+
+//get all jobs
+router.get("/jobs", jobControllers.findAllJobs);
