37dad24579924d94ebf77aa06fb8ce6e0a56bb9b
sam <shoaibur206@gmail.com>
2021-11-12 16:33:47 +0500

    imported controllers

--- api/routes/job.routes.js
+++ api/routes/job.routes.js
@@ -1,3 +1,4 @@
 import JobContrller from "../controllers/job.controllers.js";
 import express from "express";
+import jobControllers from "../controllers/job.controllers.js";
 const router = express.Router();
