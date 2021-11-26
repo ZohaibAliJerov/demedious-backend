8137e51c23dde07e106da8f68d59b71ff436554d
sam <shoaibur206@gmail.com>
2021-11-26 20:31:41 +0500

    scrolled the page

--- services/lymphklinik.js
+++ services/lymphklinik.js
@@ -6,6 +6,19 @@ const lymphklinik = async () => {
     let page = await browser.newPage();
     let url = "https://www.lymphklinik.com/karriere.html";
     await page.goto(url, { timeout: 0, waitUntil: "load" });
+
+    //scroll the page
+    await page.evaluate(() => {
+      for (let i = 0; i < 100; i++) {
+        if (
+          document.scrollingElement.scrollTop + window.innerHeight >=
+          document.scrollingElement.scrollHeight
+        ) {
+          break;
+        }
+        document.scrollingElement.scrollBy(0, 100);
+      }
+    });
   } catch (error) {
     console.log(error);
   }
