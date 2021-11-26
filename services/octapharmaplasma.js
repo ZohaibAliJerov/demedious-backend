1aabe85ca5ee286024db0f73c665f291d60991d7
sam <shoaibur206@gmail.com>
2021-11-26 11:13:45 +0500

    created a page instance

--- services/octapharmaplasma.js
+++ services/octapharmaplasma.js
@@ -3,6 +3,7 @@ import puppeteer from "puppeteer";
 const octapharmaplasma = async () => {
   try {
     let browser = await puppeteer.launch({ headless: false });
+    let page = await browser.newPage();
   } catch (error) {
     console.log(error);
   }
