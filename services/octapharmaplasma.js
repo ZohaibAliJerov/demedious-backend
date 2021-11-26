ca54b8b935b8a53f756bbcddf9a01966c8b6f982
sam <shoaibur206@gmail.com>
2021-11-26 11:26:07 +0500

    scrolled the page

--- services/octapharmaplasma.js
+++ services/octapharmaplasma.js
@@ -8,6 +8,20 @@ const octapharmaplasma = async () => {
 
     //visit the site
     await page.goto(url, { waitUntil: "load", timeout: 0 });
+
+    //scroll the page
+    await page.evaluate(() => {
+      for (let i = 0; i < 100; i++) {
+        if (
+          document.scrollingElement.scrollTop + window.innerHeight >=
+          document.scrollingElement.scrollHeight
+        ) {
+          break;
+        }
+        document.scrollingElement.scrollBy(0, 100);
+        setTimeout(1000);
+      }
+    });
   } catch (error) {
     console.log(error);
   }
