53e931d3916c73f4e94c400cc54418e1ede6b933
sam <shoaibur206@gmail.com>
2021-11-26 09:07:45 +0500

    added page url

--- services/recruiting.js
+++ services/recruiting.js
@@ -7,7 +7,8 @@ const recruiting = () => {
     try {
         let browser = await puppeteer.launch({ headless: false });
         let page = await browser.newPage();
-        
+
+        let url = "https://recruitingapp-4181.de.umantis.com/Jobs/1?lang=ger";
     } catch () {
         
     }
