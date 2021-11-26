a1dfc1dea8e243f76607cc683477fcc21b43572b
sam <shoaibur206@gmail.com>
2021-11-26 11:21:08 +0500

    added website url

--- services/octapharmaplasma.js
+++ services/octapharmaplasma.js
@@ -4,6 +4,7 @@ const octapharmaplasma = async () => {
   try {
     let browser = await puppeteer.launch({ headless: false });
     let page = await browser.newPage();
+    let url = "https://www.octapharmaplasma.de/jobs";
   } catch (error) {
     console.log(error);
   }
