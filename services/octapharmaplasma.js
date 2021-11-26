3dfc93630b814cb349c1558c38a379837bbfe1bc
sam <shoaibur206@gmail.com>
2021-11-26 11:49:05 +0500

    fixed a bug

--- services/octapharmaplasma.js
+++ services/octapharmaplasma.js
@@ -21,40 +21,38 @@ const octapharmaplasma = async () => {
         document.scrollingElement.scrollBy(0, 100);
         setTimeout(1000);
       }
-
-      //get all locations
-      let allLocations = await page.evaluate(() => {
-        return Array.from(document.querySelectorAll(".card-header > h3")).map(
-          (el) => el.innerText
-        );
-      });
-
-      //get all jobLinks
-      let jobLinks = await page.evaluate(() => {
-        let links = document.querySelectorAll("div.card-body > p > a");
-        return links ? links.map((el) => el.href) : "";
-      });
-
-      for (let jobLink of jobLinks) {
-        await page.goto(jobLink, { timeout: 0, waitUntil: "load" });
-          await page.waitForTimeout(1000);
-          
-          
-    //scroll the page
-    await page.evaluate(() => {
-      for (let i = 0; i < 100; i++) {
-        if (
-          document.scrollingElement.scrollTop + window.innerHeight >=
-          document.scrollingElement.scrollHeight
-        ) {
-          break;
-        }
-        document.scrollingElement.scrollBy(0, 100);
-        setTimeout(1000);
-      }
-          
-      }
     });
+    //get all locations
+    let allLocations = await page.evaluate(() => {
+      return Array.from(document.querySelectorAll(".card-header > h3")).map(
+        (el) => el.innerText
+      );
+    });
+
+    //get all jobLinks
+    let jobLinks = await page.evaluate(() => {
+      let links = document.querySelectorAll("div.card-body > p > a");
+      return links ? links.map((el) => el.href) : "";
+    });
+
+    for (let jobLink of jobLinks) {
+      await page.goto(jobLink, { timeout: 0, waitUntil: "load" });
+      await page.waitForTimeout(1000);
+
+      //scroll the page
+      await page.evaluate(() => {
+        for (let i = 0; i < 100; i++) {
+          if (
+            document.scrollingElement.scrollTop + window.innerHeight >=
+            document.scrollingElement.scrollHeight
+          ) {
+            break;
+          }
+          document.scrollingElement.scrollBy(0, 100);
+          setTimeout(1000);
+        }
+      });
+    }
   } catch (error) {
     console.log(error);
   }
