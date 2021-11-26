2c5aef159790effdfad6e25e6df61e827fd22913
sam <shoaibur206@gmail.com>
2021-11-26 09:08:49 +0500

    logged the error object

--- services/recruiting.js
+++ services/recruiting.js
@@ -1,15 +1,12 @@
 import puppeteer from "puppeteer";
 
-
-
 const recruiting = () => {
-    
-    try {
-        let browser = await puppeteer.launch({ headless: false });
-        let page = await browser.newPage();
+  try {
+    let browser = await puppeteer.launch({ headless: false });
+    let page = await browser.newPage();
 
-        let url = "https://recruitingapp-4181.de.umantis.com/Jobs/1?lang=ger";
-    } catch () {
-        
-    }
-}
+    let url = "https://recruitingapp-4181.de.umantis.com/Jobs/1?lang=ger";
+  } catch (error) {
+    console.log(error);
+  }
+};
