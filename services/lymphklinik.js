b886a1717e9098404fb90d500b6fb9933aaf54cb
sam <shoaibur206@gmail.com>
2021-11-26 18:06:50 +0500

    got the website link

--- services/lymphklinik.js
+++ services/lymphklinik.js
@@ -4,6 +4,7 @@ const lymphklinik = async () => {
   try {
     let browser = await puppeteer.launch({ headless });
     let page = await browser.newPage();
+    let url = "https://www.lymphklinik.com/karriere.html";
   } catch (error) {
     console.log(error);
   }
