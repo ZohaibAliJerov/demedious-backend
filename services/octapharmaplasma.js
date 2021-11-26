94d9c0ecc5298f2fc6816b5a491bcc545ec703a3
sam <shoaibur206@gmail.com>
2021-11-26 11:23:42 +0500

    visited the site

--- services/octapharmaplasma.js
+++ services/octapharmaplasma.js
@@ -5,6 +5,9 @@ const octapharmaplasma = async () => {
     let browser = await puppeteer.launch({ headless: false });
     let page = await browser.newPage();
     let url = "https://www.octapharmaplasma.de/jobs";
+
+    //visit the site
+    await page.goto(url, { waitUntil: "load", timeout: 0 });
   } catch (error) {
     console.log(error);
   }
