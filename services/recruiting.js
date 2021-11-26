84b003e91bde072383a43da0ffad148269a7019e
sam <shoaibur206@gmail.com>
2021-11-26 09:06:07 +0500

    created a browser instance

---
+++ services/recruiting.js
@@ -0,0 +1,13 @@
+import puppeteer from "puppeteer";
+
+
+
+const recruiting = () => {
+    
+    try {
+        let browser = await puppeteer.launch({ headless: false });
+    
+    } catch () {
+        
+    }
+}
