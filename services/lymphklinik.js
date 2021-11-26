e2d374e64eff2d30d41c72898f9f71f188b0041d
sam <shoaibur206@gmail.com>
2021-11-26 18:05:22 +0500

    created a browser instance

--- services/lymphklinik.js
+++ services/lymphklinik.js
@@ -2,6 +2,8 @@ import puppeteer from "puppeteer";
 
 const lymphklinik = async () => {
   try {
+    let browser = await puppeteer.launch({ headless });
+    l;
   } catch (error) {
     console.log(error);
   }
