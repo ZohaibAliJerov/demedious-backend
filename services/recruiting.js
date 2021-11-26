8c13a183bedf0a632ec457fd9f61c619922fa46f
sam <shoaibur206@gmail.com>
2021-11-26 09:27:18 +0500

    scrolled the page

--- services/recruiting.js
+++ services/recruiting.js
@@ -1,4 +1,4 @@
-import puppeteer from "puppeteer";
+import puppeteer, { TimeoutError } from "puppeteer";
 
 const recruiting = () => {
   try {
@@ -13,6 +13,20 @@ const recruiting = () => {
     });
     //wait for a while
     await page.waitForTimeout(1000);
+
+    //scroll the page
+    await page.evaluate(() => {
+      for (let i = 0; i < 100; i++) {
+        if (
+          document.scrollingElement.scrollTop + window.innerHeight >=
+          document.scrollingElement.scrollHeight
+        ) {
+          break;
+        }
+        document.scrollingElement.scrollBy(0, 100);
+        setTimeout(1000);
+      }
+    });
   } catch (error) {
     console.log(error);
   }
