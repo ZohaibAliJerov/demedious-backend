e8220928ca6f011cd2dae52a963c2b27dcfed36e
sam <shoaibur206@gmail.com>
2021-12-09 00:14:59 +0500

    added user controllers

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,3 +1,46 @@
-/**
- * user controller
- */
+import jwt from "jsonwebtoken";
+import bcryptjs from "bcryptjs";
+import User from "../models/User.model.js";
+import { validate } from "../models/User.model.js";
+
+//register controller
+export const register = async (req, res) => {
+  let newUser = new User({
+    username: req.body.username,
+    email: req.body.username,
+    password: bcryptjs.hashSync(req.body.password, 10),
+  });
+  try {
+    newUser.save().then((user) => {
+      res.status(200).send("Registered successfully!");
+    });
+  } catch (err) {
+    res.status(500).send(err);
+  }
+};
+
+export const login = async (req, res) => {
+  let user = User.find({ email: req.body.email });
+  if (!user) {
+    res.status(404).send("User Not Found!");
+  }
+
+  let passwordIsValid = bcryptjs.compareSync(req.body.password, user.password);
+  if (!passwordIsValid) {
+    res.status(401).send({
+      accessToken: null,
+      message: "Invalid emial or password",
+    });
+  }
+
+  let token = jwt.sign({ _id: user._id }, process.env.TOKEN_SECRET, {
+    expiresIn: 86400,
+  });
+
+  res.status(200).send({
+    id: user._id,
+    username: user.username,
+    email: user.email,
+    accessToken: token,
+  });
+};
