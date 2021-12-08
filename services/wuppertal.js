a76d7a54f4e03b797df5bf2eb492beca9518cd3f
sher-karim05 <karimdeveloper12@gmail.com>
2021-12-08 18:48:14 +0500

    Add job emails

--- services/wuppertal.js
+++ services/wuppertal.js
@@ -10,7 +10,7 @@ const wuppertal = async () => {
     let allJobs = [];
     let allLinks = [
       "https://www.helios-gesundheit.de/kliniken/wuppertal/unser-haus-karriere-presse/karriere/stellenangebote/",
-      "https://www.helios-gesundheit.de/kliniken/wuppertal/unser-haus-karriere-presse/karriere/stellenangebote/?tx_heliosuwstemplates_jobsearch%5Baction%5D=list&tx_heliosuwstemplates_jobsearch%5Bcontroller%5D=Job&tx_heliosuwstemplates_jobsearch%5Bpage%5D=2&cHash=dacf652f0a4905f03823ca092820ee4d",
+      "https://www.helios-gesundheit.de/kliniken/wuppertal/unser-haus-karriere-presse/karriere/stellenangebote/?tx_heliosuwstemplates_jobsearch%5Baction%5D=list&tx_heliosuwstemplates_jobsearch%5Bcontrolle    r%5D=Job&tx_heliosuwstemplates_jobsearch%5Bpage%5D=2&cHash=dacf652f0a4905f03823ca092820ee4d",
       "https://www.helios-gesundheit.de/kliniken/wuppertal/unser-haus-karriere-presse/karriere/stellenangebote/?tx_heliosuwstemplates_jobsearch%5Baction%5D=list&tx_heliosuwstemplates_jobsearch%5Bcontroller%5D=Job&tx_heliosuwstemplates_jobsearch%5Bpage%5D=3&cHash=f58d93fedde7e147e7d4f52eed43e5d1",
       "https://www.helios-gesundheit.de/kliniken/wuppertal/unser-haus-karriere-presse/karriere/stellenangebote/?tx_heliosuwstemplates_jobsearch%5Baction%5D=list&tx_heliosuwstemplates_jobsearch%5Bcontroller%5D=Job&tx_heliosuwstemplates_jobsearch%5Bpage%5D=4&cHash=81aa6a60473889e8548a205757d0dab6",
       "https://www.helios-gesundheit.de/kliniken/wuppertal/unser-haus-karriere-presse/karriere/stellenangebote/?tx_heliosuwstemplates_jobsearch%5Baction%5D=list&tx_heliosuwstemplates_jobsearch%5Bcontroller%5D=Job&tx_heliosuwstemplates_jobsearch%5Bpage%5D=5&cHash=20268dcd0ca08b577c486bba485772f4",
@@ -41,55 +41,53 @@ const wuppertal = async () => {
       await page.goto(url);
       scroll(page);
 
-      const title = await page.evaluate(() => {
-        let text = document.querySelector(".billboard-panel__body > h2");
-        return text ? text.innerText : null;
-      });
+      // const title = await page.evaluate(() => {
+      //   let text = document.querySelector(".billboard-panel__body > h2");
+      //   return text ? text.innerText : null;
+      // });
 
       //get contacts
-      let cell = await page.evaluate(() => {
+      // let cell = await page.evaluate(() => {
+      //   let text = document
+      //     .querySelector(".content-block-list__container")
+      //     .getElementsByTagName("article")[4];
+      //   return text
+      //     ? text.innerText.match(
+      //       /\+\d+.\d+.\d+|\d+.\/.\d+[-/]\d+/g
+      //       )
+      //     : null;
+      // });
+    //   // get email
+      let email = await page.evaluate(() => {
         let text = document
           .querySelector(".content-block-list__container")
           .getElementsByTagName("article")[4];
         return text
           ? text.innerText.match(
-            /\+\d+.\d+.\d+|\d+.\/.\d+[-/]\d+/g
+              /[a-zA-Z]+.[a-zA-Z-]+.[a-zA-Z]+.\[at\].[a-zA-Z-]+.[a-zA-Z.]+.[a-zA-Z]+.|[a-zA-z.]+[a-zA-z]+[-/][a-zA-z]+\@[a-zA-z]+[-/][a-zA-z.]+[a-zA-z]+/g
             )
           : null;
       });
-    //   // get email
-    //   let email = await page.evaluate(() => {
-    //     let text = document
-    //       .querySelector(".content-block-list__container")
-    //       .getElementsByTagName("article")[4];
-    //     return text
-    //       ? text.innerText.match(
-    //           /[a-zA-Z]+.[a-zA-Z-]+.[a-zA-Z]+.\[at\].[a-zA-Z-]+.[a-zA-Z.]+.[a-zA-Z]+./g
-    //         )
-    //       : null;
-    //   });
-
-    //   //   get location
-    //   let location = await page.evaluate(() => {
-    //     let text = document
-    //       .querySelector(".content-block-list")
-    //       .getElementsByTagName("article")[4];
-    //     return text
-    //       ? text.innerText.match(
-    //           /[a-zA-Z.]+.\d+.[a-zA-Z]+.\d+.[a-zA-Z]+.|[a-zA-Z]+\W{1}[a-zA-Z]+\W{1}[a-zA-Z]+.\d+[,/].\d+.[a-zA-Z]+/g
-    //         )
-    //       : null;
-    //   });
-
+      //   get location
+      // let location = await page.evaluate(() => {
+      //   let text = document
+      //     .querySelector(".content-block-list")
+      //     .getElementsByTagName("article")[4];
+      //   return text
+      //     ? text.innerText.match(
+      //         /[a-zA-Z.]+.\d+.[a-zA-Z]+.\d+.[a-zA-Z]+.|[a-zA-Z]+\W{1}[a-zA-Z]+\W{1}[a-zA-Z]+.\d+[,/].\d+.[a-zA-Z]+/g
+      //       )
+      //     : null;
+      // });
     //     //get apply link
     //     let applyLink = await page.evaluate(() => {
     //       let text = document.querySelector(".dialog__content >a");
     //       return text ? text.href : null;
     //     });
       const jobDetails = {
-        title,
-        cell,
-        // email,
+        // title,
+        // cell,
+        email,
         // location,
         // applyLink,
       };
