0ef2c6096478fb2a27e6eae800af9f371a68bc36
burhanahmedbarcha <burhanbarcha@gmail.com>
2021-12-09 17:47:57 +0500

    not completed yet due to not scraping links

--- services/frenzishkus-hos.js
+++ services/frenzishkus-hos.js
@@ -1,6 +1,6 @@
 import puppeteer from 'puppeteer'
 
-const franziskus = async () => {
+const frenzishkus = async () => {
     try {
         let browser = await puppeteer.launch({ headless: false });
         let page = await browser.newPage();
@@ -15,7 +15,7 @@ const franziskus = async () => {
             await scroll(page);
 
             const job = await page.evaluate(() => {
-                let links = Array.from(document.querySelectorAll('.alternative a')
+                let links = Array.from(document.querySelectorAll('.cell_jobtitle a')
                 ).map(el => el.href);
                 return links
             });
@@ -24,58 +24,60 @@ const franziskus = async () => {
             counter++;
         } while (counter < link);
 
-        // console.log(allJobs);
-
-        // let allJobDetails = [];
-        // for (const url of allJobs) {
-        //     await page.goto(url);
-        //     await scroll(page);
-
-        //     ///getting all the title from the links
-        //     const title = await page.evaluate(() => {
-        //         let text = document.querySelector('.emp_nr_balken h1')
-        //         return text ? text.innerText : null;
-        //     });
 
 
-        //     ///getting all the location
+        let allJobDetails = [];
+        for (const url of allJobs) {
+            await page.goto(url);
+            await scroll(page);
 
-        //     const location = await page.evaluate(() => {
-        //         let text = document.querySelector('.langid_de.menuid_0');
-        //         return text ? text.innerText.match(/[a-zA-Z.-ß]+ \d+ [|] \d+ [a-zA-Z.-ß]+/) : null;
-        //     })
-        //     /// getting all the cell ; 
-        //     const cell = await page.evaluate(() => {
-        //         let text = document.querySelector('.langid_de.menuid_0');
-        //         return text ? text.innerText.match(/\d+[-/]\d+|\d+ \d+ \d+/) : null;
-        //     })
 
-        //     /// getting all the email
-        //     const email = await page.evaluate(() => {
-        //         let text = document.querySelector('.langid_de.menuid_0');
-        //         return text ? text.innerText.match(/[a-zA-Z.]+@[a-zA-Z.-]+/) : null;
-        //     });
+            await page.waitForSelector('emp_nr_balken 1')
+            ///getting all the title from the links
+            const title = await page.evaluate(() => {
+                let text = document.querySelector('emp_nr_balken h1')
+                return text ? text.innerText : null;
+            });
 
-        //     /// getting all the applylinks
-        //     const applyLink = await page.evaluate(() => {
-        //         let text = document.querySelector('.css_button a');
-        //         return text ? text.href : null;
-        //     })
-        //     const jobDetails = {
-        //         title,
-        //         location,
-        //         cell,
-        //         email,
-        //         applyLink
-        //     }
-        //     allJobDetails.push(jobDetails);
 
-        // }
-        // await page.waitForTimeout(3000)
-        // console.log(allJobDetails)
-        // await page.close();
-        // await browser.close();
-        // return allJobDetails
+            ///getting all the location
+
+            const location = await page.evaluate(() => {
+                let text = document.querySelector('body.langid_de.menuid_0');
+                return text ? text.innerText.match(/[a-zA-Z.-ß]+ \d+ [|] \d+ [a-zA-Z.-ß]+/) : null;
+            })
+            /// getting all the cell ; 
+            const cell = await page.evaluate(() => {
+                let text = document.querySelector('body.langid_de.menuid_0')
+                return text ? text.innerText.match(/\d+[-/]\d+|\d+ \d+ \d+/) : null;
+            })
+
+            /// getting all the email
+            const email = await page.evaluate(() => {
+                let text = document.querySelector('body.langid_de.menuid_0');
+                return text ? text.innerText.match(/[a-zA-Z.]+@[a-zA-Z.-]+/) : null;
+            });
+
+            /// getting all the applylinks
+            const applyLink = await page.evaluate(() => {
+                let text = document.querySelector('.css_button a');
+                return text ? text.href : null;
+            })
+            const jobDetails = {
+                title,
+                location,
+                cell,
+                email,
+                applyLink
+            }
+            allJobDetails.push(jobDetails);
+
+        }
+        await page.waitForTimeout(3000)
+        console.log(allJobDetails)
+        await page.close();
+        await browser.close();
+        return allJobDetails
     } catch (error) {
         console.log(error)
     }
@@ -96,4 +98,104 @@ async function scroll(page) {
     });
 }
 
-franziskus();
+frenzishkus();
+
+// import puppeteer from 'puppeteer'
+
+// const franziskus = async () => {
+//     try {
+//         let browser = await puppeteer.launch({ headless: false });
+//         let page = await browser.newPage();
+//         page.setDefaultNavigationTimeout(0)
+
+//         let allJobs = [];
+//         let link = ["https://www.franziskus.de/de/beruf-karriere/alle-stellenangebote-kurzbewerbung-pflege/stellenangebote"];
+
+//         let counter = 0;
+//         do {
+//             await page.goto(link[counter], { timeout: 0 });
+//             await scroll(page);
+
+//             const job = await page.evaluate(() => {
+//                 let links = Array.from(document.querySelectorAll('.alternative a')
+//                 ).map(el => el.href);
+//                 return links
+//             });
+//             allJobs.push(...job);
+//             console.log(job);
+//             counter++;
+//         } while (counter < link);
+
+//         // console.log(allJobs);
+
+//         // let allJobDetails = [];
+//         // for (const url of allJobs) {
+//         //     await page.goto(url);
+//         //     await scroll(page);
+
+//         //     ///getting all the title from the links
+//         //     const title = await page.evaluate(() => {
+//         //         let text = document.querySelector('.emp_nr_balken h1')
+//         //         return text ? text.innerText : null;
+//         //     });
+
+
+//         //     ///getting all the location
+
+//         //     const location = await page.evaluate(() => {
+//         //         let text = document.querySelector('.langid_de.menuid_0');
+//         //         return text ? text.innerText.match(/[a-zA-Z.-ß]+ \d+ [|] \d+ [a-zA-Z.-ß]+/) : null;
+//         //     })
+//         //     /// getting all the cell ; 
+//         //     const cell = await page.evaluate(() => {
+//         //         let text = document.querySelector('.langid_de.menuid_0');
+//         //         return text ? text.innerText.match(/\d+[-/]\d+|\d+ \d+ \d+/) : null;
+//         //     })
+
+//         //     /// getting all the email
+//         //     const email = await page.evaluate(() => {
+//         //         let text = document.querySelector('.langid_de.menuid_0');
+//         //         return text ? text.innerText.match(/[a-zA-Z.]+@[a-zA-Z.-]+/) : null;
+//         //     });
+
+//         //     /// getting all the applylinks
+//         //     const applyLink = await page.evaluate(() => {
+//         //         let text = document.querySelector('.css_button a');
+//         //         return text ? text.href : null;
+//         //     })
+//         //     const jobDetails = {
+//         //         title,
+//         //         location,
+//         //         cell,
+//         //         email,
+//         //         applyLink
+//         //     }
+//         //     allJobDetails.push(jobDetails);
+
+//         // }
+//         // await page.waitForTimeout(3000)
+//         // console.log(allJobDetails)
+//         // await page.close();
+//         // await browser.close();
+//         // return allJobDetails
+//     } catch (error) {
+//         console.log(error)
+//     }
+// }
+// async function scroll(page) {
+//     await page.evaluate(() => {
+//         const distance = 100;
+//         const delay = 100;
+//         const timer = setInterval(() => {
+//             document.scrollingElement.scrollBy(0, distance);
+//             if (
+//                 document.scrollingElement.scrollTop + window.innerHeight >=
+//                 document.scrollingElement.scrollHeight
+//             ) {
+//                 clearInterval(timer);
+//             }
+//         }, delay);
+//     });
+// }
+
+// franziskus();
