e4c542b9a4347ddd5e30d6a6518432edf9f37434
sher-karim05 <karimdeveloper12@gmail.com>
2021-12-11 00:48:51 +0500

    Add job addresses

--- services/minden.js
+++ services/minden.js
@@ -36,10 +36,10 @@ const minden = async () => {
       await page.goto(url);
       scroll(page);
 
-      const title = await page.evaluate(() => {
-        let text = document.querySelector("h2")
-        return text ? text.innerText : null;
-      });
+    //   const title = await page.evaluate(() => {
+    //     let text = document.querySelector("h2")
+    //     return text ? text.innerText : null;
+    //   });
 
       //get contacts
     //   let cell = await page.evaluate(() => {
@@ -56,19 +56,16 @@ const minden = async () => {
           : null;
       });
 
-    //   // get location
-    //   await page.waitForSelector(".content-block-list");
-    //   let location = await page.evaluate(() => {
-    //     let text = document
-    //       .querySelector(".content-block-list")
-    //       .getElementsByTagName("article")[4];
-    //     return text
-    //       ? text.innerText.match(
-    //           /[a-zA-Z]+.[a-zA-Z]+.[a-zA-Z]+.\n\s[a-zA-Z-]+.[a-zA-Z-]+.[a-zA-Z]+.\d+.\n\s\d+.[a-zA-Z]+.|[a-zA-Z-]+[a-zA-Z-]+[a-zA-Z.]+.\d{2}\,.\d{5}.[a-zA-Z]+|[a-zA-Z-]+[a-zA-Z-][a-zA-Z]\W{1}[a-zA-Z].\d+\,.\d+.[a-zA-Z.]+/g
-    //         )
-    //       : null;
-
-    //   });
+      // get location
+      let location = await page.evaluate(() => {
+        let text = document
+          .querySelector("#page")
+          .getElementsByTagName("p")[2];
+        return text
+          ? text.innerText.match(
+            /[a-zA-Z]+\W{1}[a-zA-Z]+.[a-zA-Z]+\W{1}[a-zA-Z,]+.[a-zA-Z]+.[a-zA-Z,]+.[a-zA-Z-]+\W{1}[a-zA-Z]+.\d+\,.\d+.[a-zA-Z]+/g
+            ):null;
+      });
 
     //   //get apply link
     //   await page.waitForSelector(".dialog__content");
@@ -77,10 +74,10 @@ const minden = async () => {
     //     return text ? text.href : null;
     //   });
       const jobDetails = {
-        title,
+        // title,
         // cell,
-        email,
-        // location,
+        // email,
+        location,
     //     applyLink,
       };
       allJobDetails.push(jobDetails);
