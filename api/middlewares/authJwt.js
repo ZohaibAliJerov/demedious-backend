c095e0b897140be5b1ca604594f70cefb3bddc17
sam <shoaibur206@gmail.com>
2021-12-15 19:45:41 +0500

    fixed a bug

--- api/middlewares/authJwt.js
+++ api/middlewares/authJwt.js
@@ -1,15 +1,15 @@
 import jwt from "jsonwebtoken";
 
 const verifyToken = (req, res, next) => {
-  let token = req.headers["x-access-token"];
+  let token = req.header("auth-token");
 
   if (!token) {
-    res.status(403).send({ message: "No token provided" });
+    return res.status(403).send({ message: "No token provided" });
   }
 
   jwt.verify(token, process.env.TOKEN_SECRET, (err, decoded) => {
     if (err) {
-      res.status(401).send({ message: "Unauthorized" });
+      return res.status(401).send({ message: "Unauthorized" });
     }
     req._id = decoded._id;
     next();
