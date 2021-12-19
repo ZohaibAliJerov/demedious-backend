96754e93ea89da1319eb3b7003ae1814bcff7163
sam <shoaibur206@gmail.com>
2021-12-19 18:35:29 +0500

    removed services with bugs

--- services/mainService.js
+++ services/mainService.js
@@ -1,15 +1,6 @@
-import johanniter from "./johanniter.js";
 import aatalklinik from "./aatalklinik.js";
-//import katherina from "./katharina.js";
-import florence from "./florence.js";
-import klinikSorpesee from "./klinikSorpesee.js";
 const mainService = async () => {
-  return Promise.all([
-    johanniter(),
-    aatalklinik(),
-    florence(),
-    klinikSorpesee(),
-  ]).then((results) => {
+  return Promise.all([aatalklinik()]).then((results) => {
     return results.flat(1);
   });
 };
