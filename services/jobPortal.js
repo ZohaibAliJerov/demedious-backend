f436ef71b1dd93595cd9fec6fae479fae9e61f62
adnan-5 <xetalishking333@gmail.com>
2021-12-20 14:16:35 +0500

    envirnment created to get data

--- services/jobPortal.js
+++ services/jobPortal.js
@@ -3,7 +3,14 @@ import {scroll} from "./pageScroll.js";
 
 const stellenausschreibungen = async () =>{
           try {
-              
+            const browser = await puppeteer.launch({
+                headless: false
+                })
+            const page = await browser.newPage();
+           await page.goto(
+               "https://karriere.katharina-kasper-gruppe.de/stellenangebote.html?filter%5Borg_einheit%5D=16"
+               )
+            page.setDefaultNavigationTimeout(0);
         } catch (err) {
             console.error(err)
         }
