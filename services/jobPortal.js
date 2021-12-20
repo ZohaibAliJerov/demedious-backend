e570c7a1316fbb02e0de77edd4aee7b7f36dd85d
adnan-5 <xetalishking333@gmail.com>
2021-12-20 14:15:29 +0500

    try catch for error handling

--- services/jobPortal.js
+++ services/jobPortal.js
@@ -2,5 +2,9 @@ import puppeteer from "puppeteer";
 import {scroll} from "./pageScroll.js";
 
 const stellenausschreibungen = async () =>{
-
-}
+          try {
+              
+        } catch (err) {
+            console.error(err)
+        }
+    }
