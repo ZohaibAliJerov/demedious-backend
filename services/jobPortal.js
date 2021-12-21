0f416585b280a020151f33a108b77107c672139f
adnan-5 <xetalishking333@gmail.com>
2021-12-21 15:26:29 +0500

    job level and position get

--- services/jobPortal.js
+++ services/jobPortal.js
@@ -1,15 +1,18 @@
 import puppeteer from "puppeteer";
 import {scroll} from "./pageScroll.js";
 
+let positions = ["arzt", "pflege"];
+let levels = ["Facharzt", "Chefarzt", "Assistenzarzt"];
+
 const stellenausschreibungen = async () =>{
           try {
             const browser = await puppeteer.launch({
                 headless: false
                 })
             const page = await browser.newPage();
-           await page.goto(
-               "https://karriere.katharina-kasper-gruppe.de/stellenangebote.html?filter%5Borg_einheit%5D=16"
-               )
+        //    await page.goto(
+        //        "https://karriere.katharina-kasper-gruppe.de/stellenangebote.html?filter%5Borg_einheit%5D=16"
+        //        )
             page.setDefaultNavigationTimeout(0);
 
             const jobLinks = [ ];
@@ -30,12 +33,12 @@ const stellenausschreibungen = async () =>{
             console.log(jobLinks);
 
             let allDetails = [ ];
-    for(let details of jobLinks) {
-        let job = { };
-        scroll(page)
-        await page.goto(details)
+         for(let details of jobLinks) {
+         let job = { };
+         scroll(page)
+         await page.goto(details)
 
-        let title = await page.evaluate ( () => {
+         let title = await page.evaluate ( () => {
             let title = document.querySelector('.scheme-content.scheme-title > h1')
             return title ? title.innerText : null
          })
@@ -57,14 +60,35 @@ const stellenausschreibungen = async () =>{
             return locatin ? locatin.innerText : null
         })
         jobObject.location =  location;
-        
+        let text = await page.evaluate(() => {
+            return document.body.innerText;
+          });
+          //get level
+          let level = text.match(/Facharzt|Chefarzt|Assistenzarzt/);
+          let position = text.match(/arzt|pflege/);
+          job.level = level ? level[0] : "";
+          if (
+            level == "Facharzt" ||
+            level == "Chefarzt" ||
+            level == "Assistenzarzt"
+          ) {
+            jobObject.position = "artz";
+          }
     
+          if (position == "pflege" || (position == "Pflege" && !level in levels)) {
+            jobObject.position = "pflege";
+            jobObject.level = "Nicht angegeben";
+          }
+       if (!position in positions) {
+        continue;
+      }
          await page.waitForTimeout(4000)
         allDetails.push(jobObject)
-    }
-  
+     }
+     console.log(allDetails);
         await browser.close();
         // await page.close();
+        return allDetails
         } catch (err) {
             console.error(err)
         }
