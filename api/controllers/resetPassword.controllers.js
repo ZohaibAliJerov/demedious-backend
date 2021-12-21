d7042c31eefad47d6f9f9f02df02b0ff4469560b
sam <shoaibur206@gmail.com>
2021-12-21 19:36:46 +0500

    made a modification

--- api/controllers/resetPassword.controllers.js
+++ api/controllers/resetPassword.controllers.js
@@ -22,7 +22,7 @@ export const requestResetLink = async (req, res) => {
       }).save();
     }
 
-    const link = `${process.env.BASE_URL}/resetPassword/${user._id}/${token.token}`;
+    const link = `${process.env.BASE_URL}resetPassword/${user._id}/${token.token}`;
     await sendEmail(user.email, "Password reset", link);
 
     res.send("password reset link sent to your email account");
