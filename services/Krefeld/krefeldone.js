7e947872ec1da92f5a6cd76ffdbc3608c83b51ca
sher-karim05 <karimdeveloper12@gmail.com>
2021-12-23 18:30:56 +0500

    Update file

---
+++ services/Krefeld/krefeldone.js
@@ -0,0 +1,108 @@
+import puppeteer from "puppeteer";
+
+let positions = ["arzt", "pflege"];
+let levels = ["Facharzt", "Chefarzt", "Assistenzarzt"];
+
+let krefeldOne = async () => {
+  try {
+    let browser = await puppeteer.launch({
+      headless: false,
+    });
+    let page = await browser.newPage();
+
+    await page.goto(
+        "https://www.helios-gesundheit.de/kliniken/krefeld-uerdingen/unser-haus/karriere/stellenangebote/",
+        "https://www.helios-gesundheit.de/kliniken/krefeld-uerdingen/unser-haus/karriere/stellenangebote/?tx_heliosuwstemplates_jobsearch%5Baction%5D=list&tx_heliosuwstemplates_jobsearch%5Bcontroller%5D=Job&tx_heliosuwstemplates_jobsearch%5Bpage%5D=2&cHash=3ee02bc1bc73f087d3a30fe57d064ec0",
+     {
+      waitUntil: "load",
+      timeout: 0,
+    });
+
+    await scroll(page);
+
+    //get all jobLinks
+    const jobLinks = await page.evaluate(() => {
+      return Array.from(
+        document.querySelectorAll("article.tabular-list__item > a")
+      ).map((el) => el.href);
+    });
+
+    console.log(jobLinks);
+    let allJobs = [];
+
+    for (let jobLink of jobLinks) {
+      let job = {
+        title: "",
+        location: "Krefeld",
+        hospital: "Helios St. Josefshospital U ",
+        link: "",
+        level: "",
+        position: "",
+      };
+
+      await page.goto(jobLink, {
+        waitUntil: "load",
+        timeout: 0,
+      });
+
+      await page.waitForTimeout(1000);
+
+      let title = await page.evaluate(() => {
+        let ttitle = document.querySelector("h2.billboard-panel__title");
+        return ttitle ? ttitle.innerText : "";
+      });
+      job.title = title;
+
+      let text = await page.evaluate(() => {
+        return document.body.innerText;
+      });
+      //get level
+      let level = text.match(/Facharzt|Chefarzt|Assistenzarzt/);
+      let position = text.match(/arzt|pflege/);
+      job.level = level ? level[0] : "";
+      if (
+        level == "Facharzt" ||
+        level == "Chefarzt" ||
+        level == "Assistenzarzt"
+      ) {
+        job.position = "artz";  
+      }
+      if (position == "pflege" || (position == "Pflege" && !level in levels)) {
+        job.position = "pflege";
+        job.level = "Nicht angegeben";
+      }
+
+      if (!position in positions) {
+        continue;
+      }
+      let link = await page.evaluate(() => {
+        let lnk = document.querySelector(".button");
+        return lnk ? lnk.href : ""
+      });
+      job.link = link;
+      allJobs.push(job);
+    }
+    console.log(allJobs);
+    return allJobs.filter((job) => job.position != "");
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+async function scroll(page) {
+  await page.evaluate(() => {
+    const distance = 100;
+    const delay = 100;
+    const timer = setInterval(() => {
+      document.scrollingElement.scrollBy(0, distance);
+      if (
+        document.scrollingElement.scrollTop + window.innerHeight >=
+        document.scrollingElement.scrollHeight
+      ) {
+        clearInterval(timer);
+      }
+    }, delay);
+  });
+}
+krefeldOne()
+export default krefeldOne;
