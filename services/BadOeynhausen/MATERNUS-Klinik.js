577fd7b2b899c3ebd56c04c5253acd802dc80a43
adnan-5 <xetalishking333@gmail.com>
2021-12-28 15:05:16 +0500

    function created for page scrolling

---
+++ services/BadOeynhausen/MATERNUS-Klinik.js
@@ -0,0 +1,16 @@
+
+
+export async function scroll(page) {
+    await page.evaluate(() => {
+        const distance = 100;
+        const delay = 100;
+        const timer = setInterval(() => {
+            document.scrollingElement.scrollBy(0, distance);
+            if (document.scrollingElement.scrollTop + window.innerHeight >= document.scrollingElement.scrollHeight) {
+                clearInterval(timer);
+            }
+        }, delay);
+    });
+}
+
+
