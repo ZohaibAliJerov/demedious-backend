45c1856052cf3129f277c11c7172e5e9617aa16d
adnan-5 <xetalishking333@gmail.com>
2021-12-28 15:07:57 +0500

    get all jobs links

--- services/BadOeynhausen/MATERNUS-Klinik.js
+++ services/BadOeynhausen/MATERNUS-Klinik.js
@@ -10,7 +10,42 @@ let levels = [
     "Chefarzt", 
 "Assistenzarzt"
  ];
+ const klinikfur_Rehabilitation = async () =>{
+    try {
+        const browser = await puppeteer.launch({
+            headless: false
+            })
+        const page = await browser.newPage();
+       await page.goto(
+           "https://www.wirpflegen.de/karriere/stellenangebote/ort/bad-oeynhausen/v/liste/j/city/nw-Bad%2520Oeynhausen?cHash=0efd8abceef8ff54a7feac73f84f1516#joResults"
+           );
+        page.setDefaultNavigationTimeout(0);
+       
+        const jobLinks = [ ];
+        let allUrls = [
+            "https://www.wirpflegen.de/karriere/stellenangebote/ort/bad-oeynhausen/v/liste/j/city/nw-Bad%2520Oeynhausen?cHash=0efd8abceef8ff54a7feac73f84f1516#joResults"
+             ]
+            // get all jobs links 
+        for(let a=0;a<allUrls.length; a++) {
+           await page.goto(allUrls[a])
+           scroll(page);
+        let jobs = await page.evaluate( ()=>{
+            return  Array.from(document.querySelectorAll(' div.item p > a')
+            ).map((el) => el.href);
+        })
+        jobLinks.push(...jobs)
+        }
+        await page.waitForTimeout(3000);
+        console.log(jobLinks);
 
+        await browser.close();
+        // await page.close();
+        // return alljobDetails.filter((job) => job.position != "");
+      
+    } catch (err) {
+        console.error(err)
+    }
+}
 export async function scroll(page) {
     await page.evaluate(() => {
         const distance = 100;
