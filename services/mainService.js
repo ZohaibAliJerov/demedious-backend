0abded386a87cde8f399109bfe4364879e2658a9
adnan-5 <xetalishking333@gmail.com>
2021-12-29 19:52:59 +0500

    clean repo

--- services/mainService.js
+++
@@ -1,8 +0,0 @@
-import aatalklinik from "./aatalklinik.js";
-const mainService = async () => {
-  return Promise.all([aatalklinik()]).then((results) => {
-    return results.flat(1);
-  });
-};
-
-export default mainService;
