5d316b89b926847bfa663254ac21da234da4d531
BurhanAhmedBarcha <burhanbarcha@gmail.com>
2022-01-01 06:34:12 -0800

    not completed yet

--- services/celenusKarrer.js
+++ services/celenusKarrer.js
@@ -1,115 +1,124 @@
 
 import puppeteer from "puppeteer";
 
-const celenusKerriere = async () => {
-  try {
-    const browser = await puppeteer.launch({ headless: false })
-    const page = await browser.newPage();
-    page.setDefaultNavigationTimeout(0);
+let positions = ["arzt", "pflege"];
+let levels = ["Facharzt", "Chefarzt", "Assistenzarzt", "Arzt", "Oberarzt"];
 
-    let allJobs = [];
-    let allLinks = [
+let celenusKerrier = async () => {
+  try {
+    let browser = await puppeteer.launch({
+      headless: false,
+    });
+
+    let page = await browser.newPage();
+    let links = [
       'https://www.klinik-hilchenbach.de/karriere/',
       'https://www.celenus-karriere.de/jobs/aktuellejobs/aerzte/',
       'https://www.celenus-karriere.de/salvea/aktuellejobs/aerzte/'
     ]
-
-    async function scroll(page) {
-      await page.evaluate(() => {
-        const distance = 100;
-        const delay = 100;
-        const timer = setInterval(() => {
-          document.scrollingElement.scrollBy(0, distance);
-          if (
-            document.scrollingElement.scrollTop + window.innerHeight >=
-            document.scrollingElement.scrollHeight
-          ) {
-            clearInterval(timer);
-          }
-        }, delay);
-      });
-    }
-    let counter = 0;
+    let jobLinks = []
+    let counter = 0
     do {
-      await page.goto(allLinks[counter], { timeout: 0 });
-      scroll(page);
+        await page.goto(links[counter], { timeout: 0 });
+        scroll(page);
+       // get all jobs links 
+       let jobs = await page.evaluate(() => {
+              return Array.from(
+                document.querySelectorAll('.ce-bodytext > ul > li > a')
+              ).map((el) => el.href)
+            });
+            jobLinks.push(...jobs)
+      
+            counter++;
+            await page.waitForTimeout(3000);
+          } while (counter < links.length);
+         console.log(jobLinks)
+    
+  
+    let allJobs = [];
 
-      // get all jobs links 
+    for (let jobLink of jobLinks) {
+      let job = {
+        title: "",
+        location: "Hilchenbach",
+        hospital: "CELENUS Fachklinik Hilche",
+        link: "",
+        level: "",
+        position: "",
+      };
 
-      let jobs = await page.evaluate(() => {
-        return Array.from(
-          document.querySelectorAll('.ce-bodytext > ul > li > a')
-        ).map((el) => el.href)
-      });
-      allJobs.push(...jobs)
-
-      counter++;
-      await page.waitForTimeout(3000);
-    } while (counter < allLinks.length);
-    console.log(allJobs);
-
-    // getting all the data from links 
-    let allJobDetails = []
-    for (const url of allJobs) {
-      await page.goto(url);
-      scroll(page);
-      await page.waitForSelector('.nc-stelle-top > h1');
-      const title = await page.evaluate(() => {
-        return document.querySelector('.nc-stelle-top > h1').innerText || null
-      })
-      // console.log(jobTitles);
-      let address = await page.evaluate(() => {
-        return document.querySelector('.nc-company-address').innerText;
-      })
-
-      // get all emails
-
-      let email = await page.evaluate(() => {
-        return document.querySelector('.nc-company-contactperson > div a').innerText
+      await page.goto(jobLink, {
+        waitUntil: "load",
+        timeout: 0,
       });
 
-      // get all onlineApplucation 
-      let applyLink = await page.evaluate(() => {
-        return document.querySelector('.nc-action-button.nc-link-form a').href
+      await page.waitForTimeout(1000);
+
+      let title = await page.evaluate(() => {
+        let ttitle = document.querySelector(".nc-stelle-top > h1");
+        return ttitle ? ttitle.innerText : "";
       });
+      job.title = title;
 
-
-      // text = text.join(",");
-
-
-      let cell = await page.evaluate(() => {
-        let regex = /\d{5}.?\d{6}|\d{4}.?\d{3}.?\d{2}.?\d{3}|\d{5}.?\d{3}.?\d{4}|\d{5}.?\d{3}.?\d{3}|\d{6}.?\d{2}.?\d{3}|\d{4}.?\d{4}.?\d{3}|\d{5}.?\d{2}.?\d{3}|\d{3}.?\d{3}.?\d{2}.?\d{4}|\d{5}.?\d{4}.?\d{3}|\d{4}.?\d{5}.?\d{4}|\d{5}.?\d{4}|\d{4}.?\d{2}.?\d{2}.?\d{2}.?\d{2}/;
-        let text = Array.from(document.querySelectorAll("div"));
-        text = text.map(el => el.innerText);
-        let str = text.join(" ");
-        str = str.match(regex);
-        return str;
+      let text = await page.evaluate(() => {
+        return document.body.innerText;
       });
-
-      let jobDetails = {
-        title,
-        address,
-        email,
-        cell,
-        applyLink
+      //get level
+      let level = text.match(/Facharzt|Chefarzt|Assistenzarzt|Arzt|Oberarzt/);
+      let position = text.match(/arzt|pflege/);
+      job.level = level ? level[0] : "";
+      if (
+        level == "Facharzt" ||
+        level == "Chefarzt" ||
+        level == "Assistenzarzt" ||
+        level == "Arzt" ||
+        level == "Oberarzt"
+      ) {
+        job.position = "artz";
       }
-      allJobDetails.push(jobDetails);
+      if (position == "pflege" || (position == "Pflege" && !level in levels)) {
+        job.position = "pflege";
+        job.level = "Nicht angegeben";
+      }
 
+      if (!position in positions) {
+        continue;
+      }
+
+      //get link
+      let link = await page.evaluate(() => {
+        let link1 = document.querySelector('.col.col-3-1.nc-sidebar > div.nc-action-button.nc-link-form a')
+        return link1.href
+      });
+      if (typeof link == "object") {
+        job.link = link;
+      }
+      // console.log(job);
+      allJobs.push(job);
     }
-
-    await page.waitForTimeout(3000);
-
-    console.log(allJobDetails);
+    console.log(allJobs);
+    await page.close();
     await browser.close();
-    return allJobDetails
-
-
-  } catch (error) {
-    console.log(error)
+    return allJobs.filter((job) => job.position != "");
+  } catch (e) {
+    console.log(e);
   }
+};
+
+async function scroll(page) {
+  await page.evaluate(() => {
+    const distance = 100;
+    const delay = 100;
+    const timer = setInterval(() => {
+      document.scrollingElement.scrollBy(0, distance);
+      if (
+        document.scrollingElement.scrollTop + window.innerHeight >=
+        document.scrollingElement.scrollHeight
+      ) {
+        clearInterval(timer);
+      }
+    }, delay);
+  });
 }
-
-//export default celenusKerriere;
-
-celenusKerriere();
+celenusKerrier()
 
