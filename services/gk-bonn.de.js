b5fb5f2cad95ee3262fde5c0d552a13c4a2b5118
BurhanAhmedBarcha <burhanbarcha@gmail.com>
2022-01-01 07:41:17 -0800

    completed the website

--- services/gk-bonn.de.js
+++ services/gk-bonn.de.js
@@ -1,105 +1,232 @@
-import puppeteer from 'puppeteer'
 
-const gk_bonn = async () => {
-    try {
-        let browser = await puppeteer.launch({ headless: false });
-        let page = await browser.newPage();
-        page.setDefaultNavigationTimeout(0)
+import puppeteer from "puppeteer";
 
-        let allJobs = [];
-        let link = [
-            "https://www.gk-bonn.de/gkbn/bildung-karriere/stellenboerse/?pageId29534281=1#list_29534",
-            "https://www.gk-bonn.de/gkbn/bildung-karriere/stellenboerse/?pageId29534281=2#list_29534281"
-        ];
+let positions = ["arzt", "pflege"];
+let levels = ["Facharzt", "Chefarzt", "Assistenzarzt", "Arzt", "Oberarzt"];
 
-        let counter = 0;
-        do {
-            await page.goto(link[counter], { timeout: 0 });
-            await scroll(page);
-
-            const job = await page.evaluate(() => {
-                let links = Array.from(document.querySelectorAll('.listEntryTitle a')
-                ).map(el => el.href);
-                return links
-            });
-            allJobs.push(...job);
-            console.log(job);
-            counter++;
-        } while (counter < link.length);
-
-
-
-        let allJobDetails = [];
-        for (const url of allJobs) {
-            await page.goto(url);
-            await scroll(page);
-
-
-            await page.waitForSelector('h1')
-            ///getting all the title from the links
-            const title = await page.evaluate(() => {
-                let text = document.querySelector('h1')
-                return text ? text.innerText : null;
-            });
-
-
-            ///getting all the location
-
-            const location = await page.evaluate(() => {
-                let text = document.querySelector('.sectionInner');
-                return text ? text.innerText.match(/[a-zA-Z- .]+\d+. \d+ [a-zA-Z- .]+|[a-zA-Z- .]+ \d+ . \d+[\n]\d+ [a-zA-Z- .]+|[a-zA-Z- .]+[\n][a-zA-Z- .]+[/][a-zA-Z- .]+|[a-zA-Z- .]+ \d+-\d+, \d+ [a-zA-Z- .]+|[a-zA-Z -]+ [a-zA-Z -]+ \d+[.-]\d+[\n][\n]\d+ [a-zA-Z- .]+|[a-zA-Z- .ß]+\d+[\n][\n]\d+ [a-zA-Z- .]+/) : null;
-            })
-            /// getting all the cell ; 
-            const cell = await page.evaluate(() => {
-                let text = document.querySelector('.sectionInner')
-                return text ? text.innerText.match(/\d+ [/] \d+[- ]\d+|\d+[-/]\d+[-/]\d+|\d+ [/] \d+ - \d/) : null;
-            })
-
-            /// getting all the email
-            const email = await page.evaluate(() => {
-
-                let text = document.querySelector('.sectionInner');
-                return text ? text.innerText.match(/[a-zA-Z.-]+@[a-zA-Z.-]+/) : null;
-            });
-
-            /// getting all the applylinks
-            const applyLink = await page.evaluate(() => {
-                let text = document.querySelector('.onlineBewerben');
-                return text ? text.href : null;
-            })
-            const jobDetails = {
-                title,
-                location,
-                cell,
-                email,
-                applyLink
-            }
-            allJobDetails.push(jobDetails);
-
-        }
-        await page.waitForTimeout(3000)
-        console.log(allJobDetails)
-        await page.close();
-        await browser.close();
-        return allJobDetails
-    } catch (error) {
-        console.log(error)
-    }
-}
-async function scroll(page) {
-    await page.evaluate(() => {
-        const distance = 100;
-        const delay = 100;
-        const timer = setInterval(() => {
-            document.scrollingElement.scrollBy(0, distance);
-            if (
-                document.scrollingElement.scrollTop + window.innerHeight >=
-                document.scrollingElement.scrollHeight
-            ) {
-                clearInterval(timer);
-            }
-        }, delay);
+let gfo_kliniken = async () => {
+  try {
+    let browser = await puppeteer.launch({
+      headless: false,
     });
-}
 
-gk_bonn();
+    let page = await browser.newPage();
+    let links = [
+        "https://www.gk-bonn.de/gkbn/bildung-karriere/stellenboerse/?pageId29534281=1#list_29534",
+        "https://www.gk-bonn.de/gkbn/bildung-karriere/stellenboerse/?pageId29534281=2#list_29534281"      
+    ]
+    let jobLinks = []
+    let counter = 0
+    do {
+        await page.goto(links[counter], { timeout: 0 });
+        scroll(page);
+       // get all jobs links 
+       let jobs = await page.evaluate(() => {
+              return Array.from(
+                document.querySelectorAll('h2.listEntryTitle a')
+              ).map((el) => el.href)
+            });
+            jobLinks.push(...jobs)
+      
+            counter++;
+            await page.waitForTimeout(3000);
+          } while (counter < links.length);
+         console.log(jobLinks)
+    
+  
+    let allJobs = [];
+
+    for (let jobLink of jobLinks) {
+      let job = {
+        title: "",
+        location: "Bonn",
+        hospital: "Gemeinschaftskrankenhau",
+        link: "",
+        level: "",
+        position: "",
+      };
+
+      await page.goto(jobLink, {
+        waitUntil: "load",
+        timeout: 0,
+      });
+
+      await page.waitForTimeout(1000);
+
+      let title = await page.evaluate(() => {
+        let ttitle = document.querySelector("h1");
+        return ttitle ? ttitle.innerText : "";
+      });
+      job.title = title;
+
+      let text = await page.evaluate(() => {
+        return document.body.innerText;
+      });
+      //get level
+      let level = text.match(/Facharzt|Chefarzt|Assistenzarzt|Arzt|Oberarzt/);
+      let position = text.match(/arzt|pflege/);
+      job.level = level ? level[0] : "";
+      if (
+        level == "Facharzt" ||
+        level == "Chefarzt" ||
+        level == "Assistenzarzt" ||
+        level == "Arzt" ||
+        level == "Oberarzt"
+      ) {
+        job.position = "artz";
+      }
+      if (position == "pflege" || (position == "Pflege" && !level in levels)) {
+        job.position = "pflege";
+        job.level = "Nicht angegeben";
+      }
+
+      if (!position in positions) {
+        continue;
+      }
+
+      //get link
+      let link = await page.evaluate(() => {
+        let link1 = document.querySelector('.onlineBewerben')
+        return link1 ? link1.href :  null
+      });
+      job.link = link
+    
+      allJobs.push(job);
+    }
+    console.log(allJobs);
+    await page.close();
+    await browser.close();
+    return allJobs.filter((job) => job.position != "");
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+async function scroll(page) {
+  await page.evaluate(() => {
+    const distance = 100;
+    const delay = 100;
+    const timer = setInterval(() => {
+      document.scrollingElement.scrollBy(0, distance);
+      if (
+        document.scrollingElement.scrollTop + window.innerHeight >=
+        document.scrollingElement.scrollHeight
+      ) {
+        clearInterval(timer);
+      }
+    }, delay);
+  });
+}
+gfo_kliniken()
+
+
+
+
+
+
+
+// // import puppeteer from 'puppeteer'
+
+// // const gk_bonn = async () => {
+// //     try {
+// //         let browser = await puppeteer.launch({ headless: false });
+// //         let page = await browser.newPage();
+// //         page.setDefaultNavigationTimeout(0)
+
+// //         let allJobs = [];
+// //         let link = [
+// //             "https://www.gk-bonn.de/gkbn/bildung-karriere/stellenboerse/?pageId29534281=1#list_29534",
+// //             "https://www.gk-bonn.de/gkbn/bildung-karriere/stellenboerse/?pageId29534281=2#list_29534281"
+// //         ];
+
+// //         let counter = 0;
+// //         do {
+// //             await page.goto(link[counter], { timeout: 0 });
+// //             await scroll(page);
+
+// //             const job = await page.evaluate(() => {
+// //                 let links = Array.from(document.querySelectorAll('.listEntryTitle a')
+// //                 ).map(el => el.href);
+// //                 return links
+// //             });
+// //             allJobs.push(...job);
+// //             console.log(job);
+// //             counter++;
+// //         } while (counter < link.length);
+
+
+
+// //         let allJobDetails = [];
+// //         for (const url of allJobs) {
+// //             await page.goto(url);
+// //             await scroll(page);
+
+
+// //             await page.waitForSelector('h1')
+// //             ///getting all the title from the links
+// //             const title = await page.evaluate(() => {
+// //                 let text = document.querySelector('h1')
+// //                 return text ? text.innerText : null;
+// //             });
+
+
+// //             ///getting all the location
+
+// //             const location = await page.evaluate(() => {
+// //                 let text = document.querySelector('.sectionInner');
+// //                 return text ? text.innerText.match(/[a-zA-Z- .]+\d+. \d+ [a-zA-Z- .]+|[a-zA-Z- .]+ \d+ . \d+[\n]\d+ [a-zA-Z- .]+|[a-zA-Z- .]+[\n][a-zA-Z- .]+[/][a-zA-Z- .]+|[a-zA-Z- .]+ \d+-\d+, \d+ [a-zA-Z- .]+|[a-zA-Z -]+ [a-zA-Z -]+ \d+[.-]\d+[\n][\n]\d+ [a-zA-Z- .]+|[a-zA-Z- .ß]+\d+[\n][\n]\d+ [a-zA-Z- .]+/) : null;
+// //             })
+// //             /// getting all the cell ; 
+// //             const cell = await page.evaluate(() => {
+// //                 let text = document.querySelector('.sectionInner')
+// //                 return text ? text.innerText.match(/\d+ [/] \d+[- ]\d+|\d+[-/]\d+[-/]\d+|\d+ [/] \d+ - \d/) : null;
+// //             })
+
+// //             /// getting all the email
+// //             const email = await page.evaluate(() => {
+
+// //                 let text = document.querySelector('.sectionInner');
+// //                 return text ? text.innerText.match(/[a-zA-Z.-]+@[a-zA-Z.-]+/) : null;
+// //             });
+
+// //             /// getting all the applylinks
+// //             const applyLink = await page.evaluate(() => {
+// //                 let text = document.querySelector('.onlineBewerben');
+// //                 return text ? text.href : null;
+// //             })
+// //             const jobDetails = {
+// //                 title,
+// //                 location,
+// //                 cell,
+// //                 email,
+// //                 applyLink
+// //             }
+// //             allJobDetails.push(jobDetails);
+
+// //         }
+// //         await page.waitForTimeout(3000)
+// //         console.log(allJobDetails)
+// //         await page.close();
+// //         await browser.close();
+// //         return allJobDetails
+// //     } catch (error) {
+// //         console.log(error)
+// //     }
+// // }
+// // async function scroll(page) {
+// //     await page.evaluate(() => {
+// //         const distance = 100;
+// //         const delay = 100;
+// //         const timer = setInterval(() => {
+// //             document.scrollingElement.scrollBy(0, distance);
+// //             if (
+// //                 document.scrollingElement.scrollTop + window.innerHeight >=
+// //                 document.scrollingElement.scrollHeight
+// //             ) {
+// //                 clearInterval(timer);
+// //             }
+// //         }, delay);
+// //     });
+// // }
+
+// // gk_bonn();
