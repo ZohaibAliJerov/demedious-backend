83b3d232d7747eea18555b68418df9608c6e148d
BurhanAhmedBarcha <burhanbarcha@gmail.com>
2022-01-03 06:56:59 -0800

    completed this website only one link

--- services/stellenangoBot.js
+++ services/stellenangoBot.js
@@ -1,114 +1,118 @@
-import puppeteer from 'puppeteer';
 
-const stellenangobotOnline = async () => {
-    try {
+import puppeteer from "puppeteer";
 
-        let browser = await puppeteer.launch({ headless: false });
-        let page = await browser.newPage();
-        page.setDefaultNavigationTimeout(0)
+let positions = ["arzt", "pflege"];
+let levels = ["Facharzt", "Chefarzt", "Assistenzarzt", "Arzt", "Oberarzt"];
 
-        let allJobs = [];
-        let link = ["https://www.stellenangebot.online/category/orte/58769-nachrodt-wiblingwerde/"];
+let stellengBOt = async () => {
+  try {
+    let browser = await puppeteer.launch({
+      headless: false,
+    });
 
-        let counter = 0;
-        do {
-            await page.goto(link[counter], { timeout: 0 });
-            await scroll(page);
+    let page = await browser.newPage();
 
-            const job = await page.evaluate(() => {
-                let links = Array.from(document.querySelectorAll('h1.entry-title a')
-                ).map(el => el.href);
-                return links
-            });
-            allJobs.push(...job);
-            console.log(job);
-            counter++;
-        } while (counter < link.length);
+    await page.goto("https://www.stellenangebot.online/category/orte/58769-nachrodt-wiblingwerde/", {
+      waitUntil: "load",
+      timeout: 0,
+    });
 
-        let allJobDetails = [];
+    await scroll(page);
 
+    //get all jobLinks
+    const jobLinks = await page.evaluate(() => {
+      return Array.from(
+        document.querySelectorAll("h1.entry-title a")
+      ).map((el) => el.href);
+    });
 
-        for (const url of allJobs) {
-            await page.goto(url);
+    console.log(jobLinks);
+    let allJobs = [];
 
-            // await page.click('a.cc-btn.cc-deny')
+    for (let jobLink of jobLinks) {
+      let job = {
+        title: "",
+        location: "Nachrodt-Wiblingwerde",
+        hospital: "Gut Sassenscheid",
+        link: "",
+        level: "",
+        position: "",
+      };
 
-            await scroll(page);
+      await page.goto(jobLink, {
+        waitUntil: "load",
+        timeout: 0,
+      });
 
+      await page.waitForTimeout(1000);
 
-            await page.waitForSelector('#post-11306 > div > p:nth-child(3)')
-            ///getting all the title from the links
-            const title = await page.evaluate(() => {
-                let text = document.querySelector('#post-11306 > div > p:nth-child(3)')
-                return text ? text.innerText : null;
-            });
+      let title = await page.evaluate(() => {
+        let ttitle = document.querySelector("#post-11306 > div > p:nth-child(3)");
+        return ttitle ? ttitle.innerText : "";
+      });
+      job.title = title;
 
+      let text = await page.evaluate(() => {
+        return document.body.innerText;
+      });
+      //get level
+      let level = text.match(/Facharzt|Chefarzt|Assistenzarzt|Arzt|Oberarzt/);
+      let position = text.match(/arzt|pflege/);
+      job.level = level ? level[0] : "";
+      if (
+        level == "Facharzt" ||
+        level == "Chefarzt" ||
+        level == "Assistenzarzt" ||
+        level == "Arzt" ||
+        level == "Oberarzt"
+      ) {
+        job.position = "artz";
+      }
+      if (position == "pflege" || (position == "Pflege" && !level in levels)) {
+        job.position = "pflege";
+        job.level = "Nicht angegeben";
+      }
 
-            ///getting all the location
+      if (!position in positions) {
+        continue;
+      }
 
-            const location = await page.evaluate(() => {
-                let text = document.querySelector('body');
-                return text ? text.innerText.match(/[a-zA-Zßü]+ [\d,]+[\n]\d+ [a-zA-Zßü -]+|[a-zA-Zßü]+ [a-zA-Zßü]+. \d+, \d+ [a-zA-Zßü ]+|[a-zA-Zßü -]+. \d+[\n]\d+ [a-zA-Zßü ]+|[a-zA-Zßü.-]+ \d+, \d+[ [a-zA-Zßü ]+/) : null;
-            })
-            /// getting all the cell ; 
-            const cell = await page.evaluate(() => {
-                let text = document.querySelector('body')
-                return text ? text.innerText.match(/[+] \d+ [(]\d+[)] \d+ \d+-\d+/) : null;
-            })
-
-            /// getting all the email
-            const email = await page.evaluate(() => {
-                let text = document.querySelector('body');
-                return text ? text.innerText.match(/[a-zA-Z-.]+@[a-zA-Z-.]+/) : null;
-            });
-
-            /// getting all the applylinks
-            const applyLink = await page.evaluate(() => {
-                return Array.from(document.querySelectorAll("#post-11306 > div > p:nth-child(14) > a:nth-child(14)")
-                ).map(el => el.href)
-            })
-            const jobDetails = {
-                title,
-                location,
-                cell,
-                email,
-                applyLink
-            }
-            allJobDetails.push(jobDetails);
-
-        }
-        await page.waitForTimeout(3000)
-        console.log(allJobDetails)
-        await page.close();
-        await browser.close();
-        return allJobDetails
-    } catch (error) {
-        console.log(error)
+      //get link
+      let link = await page.evaluate(() => {
+        return document.body.innerText.match(/[a-zA-Z-.]+@[a-zA-Z-.]+/);
+      });
+      if (typeof link == "object") {
+        job.link = link;
+      }
+      // console.log(job);
+      allJobs.push(job);
     }
-}
-
+    console.log(allJobs)
+    await page.close();
+    await browser.close();
+    return allJobs.filter((job) => job.position != "");
+  } catch (e) {
+    console.log(e);
+  }
+};
 
 async function scroll(page) {
-    await page.evaluate(() => {
-        const distance = 100;
-        const delay = 100;
-        const timer = setInterval(() => {
-            document.scrollingElement.scrollBy(0, distance);
-            if (
-                document.scrollingElement.scrollTop + window.innerHeight >=
-                document.scrollingElement.scrollHeight
-            ) {
-                clearInterval(timer);
-            }
-        }, delay);
-    });
+  await page.evaluate(() => {
+    const distance = 100;
+    const delay = 100;
+    const timer = setInterval(() => {
+      document.scrollingElement.scrollBy(0, distance);
+      if (
+        document.scrollingElement.scrollTop + window.innerHeight >=
+        document.scrollingElement.scrollHeight
+      ) {
+        clearInterval(timer);
+      }
+    }, delay);
+  });
 }
 
-stellenangobotOnline();
-
-
-
-
+stellengBOt()
 
 
 
