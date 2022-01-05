7f07a59ae887df9c0623ae0366971020e6933502
sher-karim05 <karimdeveloper12@gmail.com>
2022-01-05 21:41:55 +0500

    Update fiels

--- services/Geseke/geseke.js
+++ services/Geseke/geseke.js
@@ -1,7 +1,7 @@
 import puppeteer from "puppeteer";
 
 let positions = ["arzt", "pflege"];
-let levels = ["Facharzt", "Chefarzt", "Assistenzarzt"];
+let levels = ["Facharzt", "Chefarzt", "Assistenzarzt",  "Arzt", "Oberarzt"];
 
 let geseke = async () => {
   try {
@@ -63,7 +63,10 @@ let geseke = async () => {
       if (
         level == "Facharzt" ||
         level == "Chefarzt" ||
-        level == "Assistenzarzt"
+        level == "Assistenzarzt"||
+        level =="Arzt"||
+        level == "Oberarzt"
+
       ) {
         job.position = "artz";
       }
