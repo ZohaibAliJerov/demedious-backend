08f7598af5f661e363810a1667c91a28619a5d7f
BurhanAhmedBarcha <burhanbarcha@gmail.com>
2022-01-05 06:19:21 -0800

    not completed

--- services/johanniter.js
+++ services/johanniter.js
@@ -1,121 +1,123 @@
-import puppeteer from "puppeteer";
 
-const johanniter = async () => {
-  try {
-    let browser = await puppeteer.launch({ headless: true });
-    let page = await browser.newPage();
-    let url =
-      "https://www.johanniter.de/johanniter-kliniken/neurologisches-rehabilitationszentrum-godeshoehe/karriere/";
 
-    await page.goto(url, { timeout: 0, waitUntil: "load" });
-    await page.waitForTimeout(3000);
-    //remove the dialog box
-    await page.waitForSelector("#uc-btn-accept-banner");
-    await page.click("#uc-btn-accept-banner");
+// import puppeteer from "puppeteer";
 
-    await page.waitForTimeout(1000);
-    //scroll the page
-    await page.evaluate(() => {
-      for (let i = 0; i < 100; i++) {
-        if (
-          document.scrollingElement.scrollTop + window.innerHeight >=
-          document.scrollingElement.scrollHeight
-        ) {
-          break;
-        }
-        document.scrollingElement.scrollBy(0, 100);
-        setTimeout(10000);
-      }
-    });
-    //get all pages
-    let pages = await page.evaluate(() => {
-      return Array.from(
-        document.querySelectorAll(".c-pagination__list > li > a")
-      ).map((el) => el.href);
-    });
+// const johanniter = async () => {
+//   try {
+//     let browser = await puppeteer.launch({ headless: true });
+//     let page = await browser.newPage();
+//     let url =
+//       "https://www.johanniter.de/johanniter-kliniken/neurologisches-rehabilitationszentrum-godeshoehe/karriere/";
 
-    //get all job links
-    let allJobLinks = [];
-    for (let pg of pages) {
-      //visit each page
-      await page.goto(`${pg}`, { timeout: 0, waitUntil: "load" });
-      //scroll the each page
-      //scroll the page
-      await page.evaluate(() => {
-        for (let i = 0; i < 100; i++) {
-          if (
-            document.scrollingElement.scrollTop + window.innerHeight >=
-            document.scrollingElement.scrollHeight
-          ) {
-            break;
-          }
-          document.scrollingElement.scrollBy(0, 100);
+//     await page.goto(url, { timeout: 0, waitUntil: "load" });
+//     await page.waitForTimeout(3000);
+//     //remove the dialog box
+//     await page.waitForSelector("#uc-btn-accept-banner");
+//     await page.click("#uc-btn-accept-banner");
 
-          setTimeout(1000);
-        }
-      });
+//     await page.waitForTimeout(1000);
+//     //scroll the page
+//     await page.evaluate(() => {
+//       for (let i = 0; i < 100; i++) {
+//         if (
+//           document.scrollingElement.scrollTop + window.innerHeight >=
+//           document.scrollingElement.scrollHeight
+//         ) {
+//           break;
+//         }
+//         document.scrollingElement.scrollBy(0, 100);
+//         setTimeout(10000);
+//       }
+//     });
+//     //get all pages
+//     let pages = await page.evaluate(() => {
+//       return Array.from(
+//         document.querySelectorAll(".c-pagination__list > li > a")
+//       ).map((el) => el.href);
+//     });
 
-      let jobLinks = await page.evaluate(() => {
-        return Array.from(
-          document.querySelectorAll("div.c-content-list__text > h3 > a")
-        ).map((el) => el.href);
-      });
-      allJobLinks.push(...jobLinks);
-    }
+//     //get all job links
+//     let allJobLinks = [];
+//     for (let pg of pages) {
+//       //visit each page
+//       await page.goto(`${pg}`, { timeout: 0, waitUntil: "load" });
+//       //scroll the each page
+//       //scroll the page
+//       await page.evaluate(() => {
+//         for (let i = 0; i < 100; i++) {
+//           if (
+//             document.scrollingElement.scrollTop + window.innerHeight >=
+//             document.scrollingElement.scrollHeight
+//           ) {
+//             break;
+//           }
+//           document.scrollingElement.scrollBy(0, 100);
 
-    // get all job details
-    let allJobs = [];
-    //visit all job links
-    for (let link of allJobLinks) {
-      await page.goto(link, { waitUntil: "load", timeout: 0 });
-      await page.waitForTimeout(3000);
-      //scroll the page
-      await page.evaluate(() => {
-        for (let i = 0; i < 100; i++) {
-          if (
-            document.scrollingElement.scrollTop + window.innerHeight >=
-            document.scrollingElement.scrollHeight
-          ) {
-            break;
-          }
-          document.scrollingElement.scrollBy(0, 100);
-          setTimeout(1000);
-        }
-      });
-      let title = await page.evaluate(() => {
-        return document.querySelector("h1").innerText;
-      });
+//           setTimeout(1000);
+//         }
+//       });
 
-      let location = await page.evaluate(() => {
-        return document.querySelector(".c-inline-list__list  > li").innerText;
-      });
+//       let jobLinks = await page.evaluate(() => {
+//         return Array.from(
+//           document.querySelectorAll("div.c-content-list__text > h3 > a")
+//         ).map((el) => el.href);
+//       });
+//       allJobLinks.push(...jobLinks);
+//     }
 
-      let cell = await page.evaluate(() => {
-        return document.body.innerText.match(/\d+\s\d+-\d+/);
-      });
-      if (typeof cell == "object" && cell != null) {
-        cell = cell[0];
-      } else if (cell == null) {
-        cell = "";
-      }
-      let email = await page.evaluate(() => {
-        return document.body.innerText.match(/\w+@\w+\.\w+/);
-      });
-      if (typeof email == "object" && email != null) {
-        email = email[0];
-      } else if (email == null) {
-        email = "";
-      }
-      let applyLink = email;
+//     // get all job details
+//     let allJobs = [];
+//     //visit all job links
+//     for (let link of allJobLinks) {
+//       await page.goto(link, { waitUntil: "load", timeout: 0 });
+//       await page.waitForTimeout(3000);
+//       //scroll the page
+//       await page.evaluate(() => {
+//         for (let i = 0; i < 100; i++) {
+//           if (
+//             document.scrollingElement.scrollTop + window.innerHeight >=
+//             document.scrollingElement.scrollHeight
+//           ) {
+//             break;
+//           }
+//           document.scrollingElement.scrollBy(0, 100);
+//           setTimeout(1000);
+//         }
+//       });
+//       let title = await page.evaluate(() => {
+//         return document.querySelector("h1").innerText;
+//       });
 
-      allJobs.push({ title, location, cell, email, applyLink });
-    }
-    await page.close();
-    await browser.close();
-    return allJobs;
-  } catch (error) {
-    console.log(error);
-  }
-};
+//       let location = await page.evaluate(() => {
+//         return document.querySelector(".c-inline-list__list  > li").innerText;
+//       });
 
-export default johanniter;
+//       let cell = await page.evaluate(() => {
+//         return document.body.innerText.match(/\d+\s\d+-\d+/);
+//       });
+//       if (typeof cell == "object" && cell != null) {
+//         cell = cell[0];
+//       } else if (cell == null) {
+//         cell = "";
+//       }
+//       let email = await page.evaluate(() => {
+//         return document.body.innerText.match(/\w+@\w+\.\w+/);
+//       });
+//       if (typeof email == "object" && email != null) {
+//         email = email[0];
+//       } else if (email == null) {
+//         email = "";
+//       }
+//       let applyLink = email;
+
+//       allJobs.push({ title, location, cell, email, applyLink });
+//     }
+//     await page.close();
+//     await browser.close();
+//     return allJobs;
+//   } catch (error) {
+//     console.log(error);
+//   }
+// };
+
+// export default johanniter;
