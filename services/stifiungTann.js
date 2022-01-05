63c29f300939622342843575331a86602bb0d251
BurhanAhmedBarcha <burhanbarcha@gmail.com>
2022-01-05 04:50:01 -0800

    completed this website and make changes

--- services/stifiungTann.js
+++ services/stifiungTann.js
@@ -1,106 +1,135 @@
+
+
+
 import puppeteer from "puppeteer";
 
-const stifiungTan = async () => {
-    try {
-        const browser = await puppeteer.launch({ headless: false });
-        const page = await browser.newPage();
-        page.setDefaultNavigationTimeout(0);
+let positions = ["arzt", "pflege"];
+let levels = ["Facharzt", "Chefarzt", "Assistenzarzt", "Arzt", "Oberarzt"];
 
-        let allJobs = [];
-        let link = ["https://stiftung-tannenhof-karriere.dvinci-easy.com/de/jobs",]
+let stifiungTan = async () => {
+  try {
+    let browser = await puppeteer.launch({
+      headless: false,
+    });
 
-        let counter = 0;
-        do {
-            await page.goto(link[counter], { timeout: 0 })
-            scroll(page);
-
-            //getting all the jobs links 
-
-
+    let page = await browser.newPage();
+            let jobLinks = []
+            let allLinks = [
+              "https://stiftung-tannenhof-karriere.dvinci-easy.com/de/jobs"
+            ]
+            let counter = 0;
+            do {
+                await page.goto(allLinks[counter], { timeout: 0 })
+                
+               await scroll(page)
+               
             await page.waitForTimeout(3000)
             await page.click('#headerStartPage > div > dvinci-layout > div > div > div:nth-child(2) > div.col-xs-12.col-sm-7.col-md-8.col-lg-9 > div.dvinci-job-list-pagination > a > span > span')
-            const jobs = await page.evaluate(() => {
-                return Array.from(
-                    document.querySelectorAll('a.dvinci-job-position.ng-binding')
-                ).map(el => el.href)
-            });
+         
+                
+                // getting all the links 
+                const links = await page.evaluate(() => {
+                    return Array.from(
+                        document.querySelectorAll('a.dvinci-job-position.ng-binding' )
+                        )
+                        .map(el => el.href)
+                });
+                // console.log(links)
+                jobLinks.push(...links);
+                counter++
+            } while (counter > allLinks.length);
+            console.log(jobLinks)
+    let allJobs = [];
 
-            console.log(jobs);
-            allJobs.push(...jobs);
-            counter++;
-        } while (counter < link);
+    for (let jobLink of jobLinks) {
+      let job = {
+        title: "",
+        location: "Gelsenkirchen",
+        hospital: "Evangelische Kliniken Gelsenkirchen GmbH",
+        link: "",
+        level: "",
+        position: "",
+      };
 
-        const allJobDetails = []
+      await page.goto(jobLink, {
+        waitUntil: "load",
+        timeout: 0,
+      });
 
-        for (const url of allJobs) {
-            await page.goto(url)
-            await scroll(page)
-            /// getting all the title
-            // await page.waitForSelector('h1')
-            const title = await page.evaluate(() => {
-                let text = document.querySelector('h1')
-                return text ? text.innerText : null;
-            });
+      await scroll(page)
 
-            /// getting all the cell no.
-            const cell = await page.evaluate(() => {
-                let text = document.querySelector('.col-md-12');
-                return text ? text.innerText.match(/\d+.\d+-\d+|[+]\d+ .\d+. \d+.-.\d+ \d+|[+]\d+ .\d+. \d+ \d+.-.\d+|[+]\d+ .\d+. \d+[-] \d+ - \d+|[+] .\d+. \d+ \d+ -\d+/) : null;
-            });
+      let title = await page.evaluate(() => {
+        let ttitle = document.querySelector("h1");
+        return ttitle ? ttitle.innerText : "";
+      });
+      job.title = title;
 
-            // getting all the links
-            const applyLink = await page.evaluate(() => {
-                let link = document.querySelector('.btn.btn-primary')
-                return link ? link.href : null;
+      let text = await page.evaluate(() => {
+        return document.body.innerText;
+      });
+      //get level
+      let level = text.match(/Facharzt|Chefarzt|Assistenzarzt|Arzt|Oberarzt/);
+      let position = text.match(/arzt|pflege/);
+      job.level = level ? level[0] : "";
+      if (
+        level == "Facharzt" ||
+        level == "Chefarzt" ||
+        level == "Assistenzarzt" ||
+        level == "Arzt" ||
+        level == "Oberarzt"
+      ) {
+        job.position = "artz";
+      }
+      if (position == "pflege" || (position == "Pflege" && !level in levels)) {
+        job.position = "pflege";
+        job.level = "Nicht angegeben";
+      }
 
-            })
-            // getting all the location from the links 
-            const location = await page.evaluate(() => {
-                let text = document.querySelector('.portalFooter');
-                return text ? text.innerText.match(/[a-zA-Z]+ [a-zA-Z. ]+\d+ [|] \d+ [a-zA-Z]+, [a-zA-Z]+|[a-zA-Z]+ [a-zA-Z.]+ \d+ [|] \d+ [a-zA-Z]+/) : null;
-            });
+      if (!position in positions) {
+        continue;
+      }
 
-            // /// getting all the emails 
-            const email = await page.evaluate(() => {
-                let text = document.querySelector('.col-md-12');
-                return text ? text.innerText.match(/[a-zA-Z.-]+[a-zA-Z]+@[a-zA-Z.-]+/) : null;
-            });
-
-
-            const jobDetails = {
-                title,
-                cell,
-                applyLink,
-                location,
-                email
-
-            };
-            allJobDetails.push(jobDetails);
-            await page.waitForTimeout(3000);
-        }
-        console.log(allJobDetails);
-        await page.close();
-        await browser.close();
-        return allJobDetails;
-    } catch (error) {
-        console.log(error)
-    }
-}
-
-async function scroll(page) {
-    await page.evaluate(() => {
-        const distance = 100;
-        const delay = 100;
-        const timer = setInterval(() => {
-            document.scrollingElement.scrollBy(0, distance);
-            if (
-                document.scrollingElement.scrollTop + window.innerHeight >=
-                document.scrollingElement.scrollHeight
-            ) {
-                clearInterval(timer);
-            }
-        }, delay);
+      //get link
+      let link = await page.evaluate(() => {
+       let applink = document.querySelector('.btn.btn-primary');
+       return applink ? applink.href : null;
     });
+      // if (typeof link == "object") {
+      //   job.link = link;
+      // }
+      // console.log(job);
+      job.link = link
+      allJobs.push(job);
+    }
+    console.log(allJobs)
+    await page.close();
+    await browser.close();
+    return allJobs.filter((job) => job.position != "");
+  } catch (e) {
+    console.log(e);
+  }
 };
 
-stifiungTan();
+async function scroll(page) {
+  await page.evaluate(() => {
+    const distance = 100;
+    const delay = 100;
+    const timer = setInterval(() => {
+      document.scrollingElement.scrollBy(0, distance);
+      if (
+        document.scrollingElement.scrollTop + window.innerHeight >=
+        document.scrollingElement.scrollHeight
+      ) {
+        clearInterval(timer);
+      }
+    }, delay);
+  });
+}
+
+stifiungTan()
+
+
+
+
+
+
