6a5826f572ea4fd8a5b6d4c9679f7ada5c3b0c3b
sher-karim05 <karimdeveloper12@gmail.com>
2022-01-06 15:26:17 +0500

    Update file

---
+++ services/schmallenberg/schmallenberg.js
@@ -0,0 +1,108 @@
+import puppeteer from "puppeteer";
+
+let positions = ["arzt", "pflege"];
+let levels = ["Facharzt", "Chefarzt", "Assistenzarzt","Arzt", "Oberarzt"];
+
+let schmallenberg = async () => {
+  try {
+    let browser = await puppeteer.launch({
+      headless: false,
+    });
+    let page = await browser.newPage();
+
+    await page.goto("https://www.johannesbad-karriere.com/Stellenangebote.aspx", {
+      waitUntil: "load",
+      timeout: 0,
+    });
+
+    await scroll(page);
+
+    //get all jobLinks
+    const jobLinks = await page.evaluate(() => {
+      return Array.from(document.querySelectorAll("a.position-title")).map(
+        (el) => el.href
+      );
+    });
+
+    console.log(jobLinks);
+    let allJobs = [];
+
+    for (let jobLink of jobLinks) {
+      let job = {
+        title: "",
+        location: "Schmallenberg",
+        hospital: "Johannesbad Fachklinik  Fredeburg",
+        link: "",
+        level: "",
+        position: "",
+      };
+
+      await page.goto(jobLink, {
+        waitUntil: "load",
+        timeout: 0,
+      });
+
+      await page.waitForTimeout(1000);
+
+      let title = await page.evaluate(() => {
+        let ttitle = document.querySelector("div.content-container > h1");
+        return ttitle ? ttitle.innerText : "";
+      });
+      job.title = title;
+
+      let text = await page.evaluate(() => {
+        return document.body.innerText;
+      });
+      //get level
+      let level = text.match(/Facharzt|Chefarzt|Assistenzarzt/);
+      let position = text.match(/arzt|pflege/);
+      job.level = level ? level[0] : "";
+      if (
+        level == "Facharzt" ||
+        level == "Chefarzt" ||
+        level == "Assistenzarzt"||
+        level =="Arzt"||
+        level == "Oberarzt"
+
+      ) {
+        job.position = "artz";
+      }
+      if (position == "pflege" || (position == "Pflege" && !level in levels)) {
+        job.position = "pflege";
+        job.level = "Nicht angegeben";
+      }
+
+      if (!position in positions) {
+        continue;
+      }
+      let link = await page.evaluate(() => {
+        let lnk = document.querySelector("a#ctl01_cphInhalt_hBewerben1");
+        return lnk ? lnk.href : "";
+      });
+      job.link = link;
+      allJobs.push(job);
+    }
+    console.log(allJobs);
+    return allJobs.filter((job) => job.position != "");
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+async function scroll(page) {
+  await page.evaluate(() => {
+    const distance = 100;
+    const delay = 100;
+    const timer = setInterval(() => {
+      document.scrollingElement.scrollBy(0, distance);
+      if (
+        document.scrollingElement.scrollTop + window.innerHeight >=
+        document.scrollingElement.scrollHeight
+      ) {
+        clearInterval(timer);
+      }
+    }, delay);
+  });
+}
+schmallenberg();
+export default schmallenberg;
