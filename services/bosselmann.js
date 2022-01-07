c1140e3a5819602e0bd91b8ae947b5824852a7e7
sam <shoaibur206@gmail.com>
2022-01-07 19:58:24 +0500

    modify url

---
+++ services/bosselmann.js
@@ -0,0 +1,87 @@
+import puppeteer from "puppeteer";
+
+let positions = ["arzt", "pflege"];
+let levels = ["Facharzt", "Chefarzt", "Assistenzarzt", "Arzt", "Oberarzt"];
+
+const paracelsus = async () => {
+  let browser = await puppeteer.launch({ headless: false });
+  let page = await browser.newPage();
+
+  let url =
+    "https://www.bosselmann-siepe.de/praxisklinik-bonn/stellenanzeigen/";
+
+  await page.goto(url, { timeout: 0, waitUntil: "load" });
+
+  //scroll the page
+  await scroll(page);
+  //get all links
+  let links = await page.evaluate(() => {
+    return Array.from(
+      document.querySelectorAll(".module.moduleItems.hasShonts > a ")
+    ).map((el) => el.href);
+  });
+  //get all job details
+  let allJobs = [];
+  for (let link of links) {
+    await page.goto(link, { timeout: 0, waitUntil: "load" });
+    await page.waitForTimeout(5000);
+    let job = {
+      title: "",
+      location: "Hemer",
+      hospital: "Paracelsus-Klinik Hemer",
+      link: "",
+      level: "",
+      position: "",
+    };
+    job.title = await page.evaluate(() => {
+      return document.querySelector("h1").innerText;
+    });
+    let text = await page.evaluate(() => {
+      return document.body.innerText;
+    });
+    //get level and positions
+    let level = text.match(/Facharzt|Chefarzt|Assistenzarzt|Arzt|Oberarzt/);
+    let position = text.match(/arzt|pflege/);
+    job.level = level ? level[0] : "";
+    if (
+      level == "Facharzt" ||
+      level == "Chefarzt" ||
+      level == "Assistenzarzt" ||
+      level == "Arzt" ||
+      level == "Oberarzt"
+    ) {
+      job.position = "artz";
+    }
+    if (position == "pflege" || (position == "Pflege" && !level in levels)) {
+      job.position = "pflege";
+      job.level = "Nicht angegeben";
+    }
+
+    if (!position in positions) {
+      continue;
+    }
+    job.link = link;
+    allJobs.push(job);
+  } //end of for loop
+  await page.close();
+  await browser.close();
+  return allJobs;
+};
+
+async function scroll(page) {
+  await page.evaluate(() => {
+    const distance = 100;
+    const delay = 100;
+    const timer = setInterval(() => {
+      document.scrollingElement.scrollBy(0, distance);
+      if (
+        document.scrollingElement.scrollTop + window.innerHeight >=
+        document.scrollingElement.scrollHeight
+      ) {
+        clearInterval(timer);
+      }
+    }, delay);
+  });
+}
+
+export default paracelsus;
