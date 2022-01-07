50b99f9212267cf2b892c173258ec4b4b99f9ed9
BurhanAhmedBarcha <burhanbarcha@gmail.com>
2022-01-07 06:56:38 -0800

    completed and make some new change to js file

--- services/eko.js
+++ services/eko.js
@@ -1,105 +1,116 @@
+
 import puppeteer from "puppeteer";
 
-const kalkon = async () => {
-    try {
-        const browser = await puppeteer.launch({ headless: false });
-        const page = await browser.newPage();
-        page.setDefaultNavigationTimeout(0);
+let positions = ["arzt", "pflege"];
+let levels = ["Facharzt", "Chefarzt", "Assistenzarzt", "Arzt", "Oberarzt"];
 
-        let allJobs = [];
-        let link = ["https://eko.de/unternehmen/stellenangebote.html"]
-
-        let counter = 0;
-        do {
-            await page.goto(link[counter], { timeout: 0 })
-            await page.click('.vbcn-button.js-analytics-track');
-            // await page.waitForTimeout(3000);
-            // await page.waitForSelector('.job-title')
-            await scroll(page);
-
-
-
-            //getting all the jobs links 
-            const jobs = await page.evaluate(() => {
-                return Array.from(
-                    document.querySelectorAll('.listitemtitle a')
-                ).map(el => el.href)
-            });
-            console.log(jobs);
-            allJobs.push(...jobs);
-            counter++;
-        } while (counter < link);
-
-        const allJobDetails = []
-
-        for (const url of allJobs) {
-            await page.goto(url)
-            await page.click('.vbcn-button.js-analytics-track');
-            await scroll(page)
-            /// getting all the title
-
-            const title = await page.evaluate(() => {
-                let text = document.querySelector('.jobmodul h1')
-                return text ? text.innerText : null;
-            })
-
-            /// getting all the cell no.
-            const cell = await page.evaluate(() => {
-                let text = document.querySelector('.jobmodul');
-                return text ? text.innerText.match(/\d+[/] \d+ \d+|\d+ \d+[-]\d+|\d+.\d+ - \d+/) : null;
-            });
-
-            // getting all the location from the links 
-            const location = await page.evaluate(() => {
-                let text = document.querySelector('body');
-                return text ? text.innerText.match(/[a-zA-Z]+. \d+ [|] \d+ [a-zA-Z,]+ [a-zA-Z]+|[a-zA-Z]+. \d+ [|] \d+ [a-zA-Z.]+|[a-zA-Z.]+ .[a-zA-Z.]+. [a-zA-Z.]+ .[a-zA-Z.]+. [a-zA-Z.]+/) : null;
-            });
-
-            /// getting all the emails 
-            const email = await page.evaluate(() => {
-                let text = document.querySelector('body');
-                return text ? text.innerText.match(/[a-zA-Z.]+ [(][a-zA-Z.]+[)] [a-zA-Z.]+ [(][a-zA-Z.]+[)] [a-zA-Z]+/) : null;
-            });
-
-            // getting all the applylinks
-            const applyLink = await page.evaluate(() => {
-                let text = document.querySelector('.applylink a');
-                return text ? text.href : null;
-            })
-
-            const jobDetails = {
-                title,
-                cell,
-                location,
-                email,
-                applyLink
-            };
-            allJobDetails.push(jobDetails);
-            await page.waitForTimeout(3000);
-        }
-        console.log(allJobDetails);
-        await page.close();
-        await browser.close();
-        return allJobDetails;
-    } catch (error) {
-        console.log(error)
-    }
-}
-
-async function scroll(page) {
-    await page.evaluate(() => {
-        const distance = 100;
-        const delay = 100;
-        const timer = setInterval(() => {
-            document.scrollingElement.scrollBy(0, distance);
-            if (
-                document.scrollingElement.scrollTop + window.innerHeight >=
-                document.scrollingElement.scrollHeight
-            ) {
-                clearInterval(timer);
-            }
-        }, delay);
+let EKO = async () => {
+  try {
+    let browser = await puppeteer.launch({
+      headless: false,
     });
+
+    let page = await browser.newPage();
+
+    await page.goto("https://eko.de/unternehmen/stellenangebote.html", {
+      waitUntil: "load",
+      timeout: 0,
+    });
+
+    await scroll(page);
+
+    //get all jobLinks
+    const jobLinks = await page.evaluate(() => {
+      return Array.from(
+        document.querySelectorAll("span.listitemtitle a")
+      ).map((el) => el.href);
+    });
+
+    console.log(jobLinks);
+    let allJobs = [];
+
+    for (let jobLink of jobLinks) {
+      let job = {
+        title: "",
+        location: "Oberhausen",
+        hospital: "Evangelisches Krankenhaus Oberhausen",
+        link: "",
+        level: "",
+        position: "",
+      };
+
+      await page.goto(jobLink, {
+        waitUntil: "load",
+        timeout: 0,
+      });
+
+      await page.waitForTimeout(1000);
+
+      let title = await page.evaluate(() => {
+        let ttitle = document.querySelector(".jobmodul > div h2");
+        return ttitle ? ttitle.innerText : "";
+      });
+      job.title = title;
+
+      let text = await page.evaluate(() => {
+        return document.body.innerText;
+      });
+      //get level
+      let level = text.match(/Facharzt|Chefarzt|Assistenzarzt|Arzt|Oberarzt/);
+      let position = text.match(/arzt|pflege/);
+      job.level = level ? level[0] : "";
+      if (
+        level == "Facharzt" ||
+        level == "Chefarzt" ||
+        level == "Assistenzarzt" ||
+        level == "Arzt" ||
+        level == "Oberarzt"
+      ) {
+        job.position = "artz";
+      }
+      if (position == "pflege" || (position == "Pflege" && !level in levels)) {
+        job.position = "pflege";
+        job.level = "Nicht angegeben";
+      }
+
+      if (!position in positions) {
+        continue;
+      }
+
+      //get link
+      let link = await page.evaluate(() => {
+          let links = document.querySelector('span.applylink a');
+        return links ? links.href : "";
+        // innerText.match(/[a-zA-Z-.]+@[a-zA-Z-.]+/) : "";
+      })
+      
+      job.link = link
+      allJobs.push(job);
+    }
+    console.log(allJobs)
+    await page.close();
+    await browser.close();
+    return allJobs.filter((job) => job.position != "");
+  } catch (e) {
+    console.log(e);
+  }
 };
 
-kalkon();
+async function scroll(page) {
+  await page.evaluate(() => {
+    const distance = 100;
+    const delay = 100;
+    const timer = setInterval(() => {
+      document.scrollingElement.scrollBy(0, distance);
+      if (
+        document.scrollingElement.scrollTop + window.innerHeight >=
+        document.scrollingElement.scrollHeight
+      ){
+          clearInterval(timer)
+      }
+    }, delay)
+});
+}
+EKO()
+
+
