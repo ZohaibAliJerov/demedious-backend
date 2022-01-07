ccbb6d2882bfa8c5b4c275e1660747c5cfa22e7c
sam <shoaibur206@gmail.com>
2022-01-07 15:24:42 +0500

    add scroll function

--- services/paracelsus.js
+++ services/paracelsus.js
@@ -14,3 +14,19 @@ const paracelsus = async () => {
   await page.close();
   await browser.close();
 };
+
+async function scroll(page) {
+  await page.evaluate(() => {
+    const distance = 100;
+    const delay = 100;
+    const timer = setInterval(() => {
+      document.scrollingElement.scrollBy(0, distance);
+      if (
+        document.scrollingElement.scrollTop + window.innerHeight >=
+        document.scrollingElement.scrollHeight
+      ) {
+        clearInterval(timer);
+      }
+    }, delay);
+  });
+}
