e00cc9073e1a625033227146cb604ae00a26e9c9
sher-karim05 <karimdeveloper12@gmail.com>
2022-01-08 14:36:11 +0500

    Update file

---
+++ services/krankenhaus/karakenhaus.js
@@ -0,0 +1,108 @@
+import puppeteer from "puppeteer";
+
+let positions = ["arzt", "pflege"];
+let levels = ["Facharzt", "Chefarzt", "Assistenzarzt", "Arzt", "Oberarzt"];
+
+let karankenhaus = async () => {
+  try {
+    let browser = await puppeteer.launch({
+      headless: false,
+    });
+    let page = await browser.newPage();
+
+    await page.goto("https://www.severinskloesterchen-karriere.de/stellenangebote/", {
+      waitUntil: "load",
+      timeout: 0,
+    });
+
+    await scroll(page);
+
+    //get all jobLinks
+    const jobLinks = await page.evaluate(() => {
+        let loadMore = document.querySelector(".button-more.load-list")
+        loadMore.click();
+      return Array.from(document.querySelectorAll("div.job-offers-list > ul > li > a")).map(
+        (el) => el.href
+      );
+    });
+    console.log(jobLinks);
+    let allJobs = [];
+
+    for (let jobLink of jobLinks) {
+      let job = {
+        title: "",
+        location: "karakenhaus der  Augustinerinnen",
+        hospital: "severinskloesterchen",
+        link: "",
+        level: "",
+        position: "",
+      };
+
+      await page.goto(jobLink, {
+        waitUntil: "load",
+        timeout: 0,
+      });
+
+      await page.waitForTimeout(1000);
+
+      let title = await page.evaluate(() => {
+        let ttitle = document.querySelector("div.bewerbung-title > h1");
+        return ttitle ? ttitle.innerText : "";
+      });
+      job.title = title;
+
+      let text = await page.evaluate(() => {
+        return document.body.innerText;
+      });
+      //get level
+      let level = text.match(/Facharzt|Chefarzt|Assistenzarzt/);
+      let position = text.match(/arzt|pflege/);
+      job.level = level ? level[0] : "";
+      if (
+        level == "Facharzt" ||
+        level == "Chefarzt" ||
+        level == "Assistenzarzt"
+      ) {
+        job.position = "artz";
+      }
+      if (position == "pflege" || (position == "Pflege" && !level in levels)) {
+        job.position = "pflege";
+        job.level = "Nicht angegeben";
+      }
+
+      if (!position in positions) {
+        continue;
+      }
+      let link = await page.evaluate(() => {
+        let lnk = document.querySelector(
+          "a.button-jetzt-bewerben"
+        );
+        return lnk ? lnk.href : "";
+      });
+      job.link = link;
+      allJobs.push(job);
+    }
+    console.log(allJobs);
+    return allJobs.filter((job) => job.position != "");
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+async function scroll(page) {
+  await page.evaluate(() => {
+    const distance = 100;
+    const delay = 100;
+    const timer = setInterval(() => {
+      document.scrollingElement.scrollBy(0, distance);
+      if (
+        document.scrollingElement.scrollTop + window.innerHeight >=
+        document.scrollingElement.scrollHeight
+      ) {
+        clearInterval(timer);
+      }
+    }, delay);
+  });
+}
+karankenhaus();
+export default karankenhaus;
