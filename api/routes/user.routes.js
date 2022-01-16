32cb1cca68c5840695f4cb8fa23f3123f6809a63
sam <shoaibur206@gmail.com>
2022-01-16 21:40:33 +0500

    add routes

--- api/routes/user.routes.js
+++ api/routes/user.routes.js
@@ -1,10 +1,4 @@
-import {
-  register,
-  login,
-  logout,
-  saveJob,
-  getSavedJobs,
-} from "../controllers/user.controller.js";
+import { register, login, Logout,saveJob, getSavedJobs } from "../controllers/user.controller.js";
 import verifySignUp from "../middlewares/verifySignup.js";
 
 import express from "express";
@@ -16,8 +10,7 @@ router.post("/signin", login);
 
 router.get("/logout", logout);
 
-router.post("/saveJob", saveJob);
+router.post("/saveJob, saveJob);
 
 router.get("/getSavedJobs", getSavedJobs);
-
 export default router;
