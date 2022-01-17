31ace691a4c989e4db1c7e10514aa98d4e9f3c7b
sher-karim05 <karimdeveloper12@gmail.com>
2022-01-17 19:46:03 +0500

    Update new changes

---
+++ services/karriere/karriere.js
@@ -0,0 +1,127 @@
+import puppeteer from "puppeteer";
+
+let positions = ["arzt", "pflege"];
+let levels = ["Facharzt", "Chefarzt", "Assistenzarzt", "Arzt", "Oberarzt"];
+
+let karriere = async () => {
+  try {
+    let browser = await puppeteer.launch({
+      headless: false,
+    });
+
+    let page = await browser.newPage();
+
+    await page.goto("https://www.koe-aesthetics.de/karriere/#d", {
+      waitUntil: "load",
+      timeout: 0,
+    });
+
+    await scroll(page);
+
+    //get all jobLinks
+    const jobLinks = await page.evaluate(() => {
+      return Array.from(
+        document.querySelectorAll(".behandlungen-inhaltsverzeichnis__listing > a")
+      ).map((el) => el.href);
+    });
+
+    console.log(jobLinks);
+    let allJobs = [];
+
+    for (let jobLink of jobLinks) {
+      let job = {
+        title: "",
+        location: "karriere",
+        city : "",
+        hospital: "koe-aesthetics",
+        link: "",
+        email: "",
+        level: "",
+        position: "",
+      };
+
+      await page.goto(jobLink, {
+        waitUntil: "load",
+        timeout: 0,
+      });
+
+      await page.waitForTimeout(1000);
+
+      let title = await page.evaluate(() => {
+        let ttitle = document.querySelector("div.block-content.text > h2");
+        return ttitle ? ttitle.innerText : "";
+      });
+      job.title = title;
+
+      let text = await page.evaluate(() => {
+        return document.body.innerText;
+      });
+      //get level
+      let level = text.match(/Facharzt|Chefarzt|Assistenzarzt|Arzt|Oberarzt/);
+      let position = text.match(/arzt|pflege/);
+      job.level = level ? level[0] : "";
+      if (
+        level == "Facharzt" ||
+        level == "Chefarzt" ||
+        level == "Assistenzarzt" ||
+        level == "Arzt" ||
+        level == "Oberarzt"
+      ) {
+        job.position = "artz";
+      }
+      if (position == "pflege" || (position == "Pflege" && !level in levels)) {
+        job.position = "pflege";
+        job.level = "Nicht angegeben";
+      }
+
+      if (!position in positions) {
+        continue;
+      }
+
+      //get link
+      let link = await page.evaluate(() => {
+          let applyLink = document.querySelector(".behandlungen-inhaltsverzeichnis__listing > a");
+          return applyLink ? applyLink.href : null;
+      });
+      job.link = link
+
+      //get city 
+      let city = await page.evaluate(()=>{
+        let cty =  document.querySelector('.footer-block')
+        return cty ? cty.innerText.slice(97,-54) : null
+      })
+      job.city = city;
+
+      //get email 
+
+      let email = await page.evaluate(()=> {
+        let eml = document.querySelector('.footer-block');
+        return eml ? eml.innerText.match(/\w+@\w+\-\w+.\w+/).toString() : null;
+      })
+      job.email = email;
+      allJobs.push(job);
+    }
+    console.log(allJobs);
+    return allJobs.filter((job) => job.position != "");
+} catch (e) {
+    console.log(e);
+}
+};
+
+async function scroll(page) {
+  await page.evaluate(() => {
+    const distance = 100;
+    const delay = 100;
+    const timer = setInterval(() => {
+      document.scrollingElement.scrollBy(0, distance);
+      if (
+        document.scrollingElement.scrollTop + window.innerHeight >=
+        document.scrollingElement.scrollHeight
+      ) {
+        clearInterval(timer);
+      }
+    }, delay);
+  });
+}
+karriere()
+export default karriere;
