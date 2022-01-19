cb95ec40f74502d93f90d248afa426abb0e74d9a
sam <shoaibur206@gmail.com>
2022-01-19 22:03:22 +0500

    modify mainService.js

--- services/mainService.js
+++ services/mainService.js
@@ -1,13 +1,13 @@
-import aatalklinik from "./aatalklinik.js";
-import krankenhausAugustin from "./krankenhausAugustin.js";
-import krakehan from "./krakenhaus.js";
+// import aatalklinik from "./aatalklinik.js";
+// import krankenhausAugustin from "./krankenhausAugustin.js";
+// import krakehan from "./krakenhaus.js";
+// import dusseldorf from "./dusseldorf.js";
+import johanniter from "./johanniter.js";
 
 const mainService = async () => {
-  return Promise.all([aatalklinik(), krakehan(), krankenhausAugustin()]).then(
-    (results) => {
-      return results.flat(1);
-    }
-  );
+  return Promise.all([johanniter()]).then((results) => {
+    return results.flat(1);
+  });
 };
 
 export default mainService;
