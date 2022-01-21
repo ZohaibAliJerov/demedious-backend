559e452570e6fc72fd0114391c8f85c1294000cb
BurhanAhmedBarcha <burhanbarcha@gmail.com>
2022-01-21 07:25:14 -0800

    not completed the website

--- services/fliednerDe.js
+++ services/fliednerDe.js
@@ -3,7 +3,7 @@ import puppeteer from "puppeteer";
 let positions = ["arzt", "pflege"];
 let levels = ["Facharzt", "Chefarzt", "Assistenzarzt", "Arzt", "Oberarzt"];
 
-let fliednerDe = async () => {
+let zissedorf = async () => {
   try {
     let browser = await puppeteer.launch({
       headless: false,
@@ -31,11 +31,14 @@ let fliednerDe = async () => {
     for (let jobLink of jobLinks) {
       let job = {
         title: "",
-        location: "Düsseldorf",
-        hospital: "Fliedner Klinik Düsseldorf",
+        location: "Gut Zissendorf, 53773 Hennef (Sieg), Germany",
+        hospital: "Fachklink Gut Zissendorf",
         link: "",
         level: "",
         position: "",
+        city: "Hennef, Sieg",
+        email: "",
+        republic: "North Rhine-Westphalia",
       };
 
       await page.goto(jobLink, {
@@ -50,6 +53,11 @@ let fliednerDe = async () => {
         return ttitle ? ttitle.innerText : "";
       });
       job.title = title;
+
+      job.location = await page.evaluate(() => {
+        let loc = document.querySelector(".con-content.col-sm-8 col-xs-12")
+        return loc ? loc.innerText.match(/[a-zA-Z-.]+[(]\w+[)][a-zA-Z-.]+|[a-zA-Z-.].+ \d+[\n]\d+[a-zA-Z-. ].+/) : '';
+      });
 
       let text = await page.evaluate(() => {
         return document.body.innerText;
@@ -76,18 +84,34 @@ let fliednerDe = async () => {
         continue;
       }
 
-      //get link
-      let link = await page.evaluate(() => {
-        let applink = document.querySelector('.a-full-area')
-        return applink ? applink.href : " ";
+      //get link\
+
+      job.email = await page.evaluate(() => {
+        return document.body.innerText.match(/[a-zA-Z-.]+@[a-zA-Z-.]+/);
       });
-      
-    job.link = link
-    allJobs.push(job);
+      if(typeof job.email == "object"){
+        job.email = email[0]
+      }
+      // job.email = email
+
+      // get link 
+      let link1 = 0;
+      if (link1) {
+        const link = await page.evaluate(() => {
+          let applyLink = document.querySelector('.a-full-area')
+          return applyLink ? applyLink.href : ""
+        })
+        job.link = link;
+      } else {
+        job.link = jobLink
+      }
+
+
+
+      allJobs.push(job);
     }
-    console.log(allJobs);
+    console.log(allJobs)
     await browser.close();
-    await page.close();
     return allJobs.filter((job) => job.position != "");
   } catch (e) {
     console.log(e);
@@ -109,6 +133,120 @@ async function scroll(page) {
     }, delay);
   });
 }
+zissedorf()
+// export default zissedorf;
 
-// fliednerDe()
-export default fliednerDe;
+// import puppeteer from "puppeteer";
+
+// let positions = ["arzt", "pflege"];
+// let levels = ["Facharzt", "Chefarzt", "Assistenzarzt", "Arzt", "Oberarzt"];
+
+// let fliednerDe = async () => {
+//   try {
+//     let browser = await puppeteer.launch({
+//       headless: false,
+//     });
+
+//     let page = await browser.newPage();
+
+//     await page.goto("https://www.fliedner.de/de/jobangebote.php", {
+//       waitUntil: "load",
+//       timeout: 0,
+//     });
+
+//     await scroll(page);
+
+//     //get all jobLinks
+//     const jobLinks = await page.evaluate(() => {
+//       return Array.from(
+//         document.querySelectorAll("span.hl3 a")
+//       ).map((el) => el.href);
+//     });
+
+//     console.log(jobLinks);
+//     let allJobs = [];
+
+//     for (let jobLink of jobLinks) {
+//       let job = {
+//         title: "",
+//         location: "Düsseldorf",
+//         hospital: "Fliedner Klinik Düsseldorf",
+//         link: "",
+//         level: "",
+//         position: "",
+//       };
+
+//       await page.goto(jobLink, {
+//         waitUntil: "load",
+//         timeout: 0,
+//       });
+
+//       await page.waitForTimeout(1000);
+
+//       let title = await page.evaluate(() => {
+//         let ttitle = document.querySelector("span.hl2");
+//         return ttitle ? ttitle.innerText : "";
+//       });
+//       job.title = title;
+
+//       let text = await page.evaluate(() => {
+//         return document.body.innerText;
+//       });
+//       //get level
+//       let level = text.match(/Facharzt|Chefarzt|Assistenzarzt|Arzt|Oberarzt/);
+//       let position = text.match(/arzt|pflege/);
+//       job.level = level ? level[0] : "";
+//       if (
+//         level == "Facharzt" ||
+//         level == "Chefarzt" ||
+//         level == "Assistenzarzt" ||
+//         level == "Arzt" ||
+//         level == "Oberarzt"
+//       ) {
+//         job.position = "artz";
+//       }
+//       if (position == "pflege" || (position == "Pflege" && !level in levels)) {
+//         job.position = "pflege";
+//         job.level = "Nicht angegeben";
+//       }
+
+//       if (!position in positions) {
+//         continue;
+//       }
+
+//       //get link
+//       let link = await page.evaluate(() => {
+//         let applink = document.querySelector('.a-full-area')
+//         return applink ? applink.href : " ";
+//       });
+      
+//     job.link = link
+//     allJobs.push(job);
+//     }
+//     console.log(allJobs);
+//     await browser.close();
+//     await page.close();
+//     return allJobs.filter((job) => job.position != "");
+//   } catch (e) {
+//     console.log(e);
+//   }
+// };
+
+// async function scroll(page) {
+//   await page.evaluate(() => {
+//     const distance = 100;
+//     const delay = 100;
+//     const timer = setInterval(() => {
+//       document.scrollingElement.scrollBy(0, distance);
+//       if (
+//         document.scrollingElement.scrollTop + window.innerHeight >=
+//         document.scrollingElement.scrollHeight
+//       ) {
+//         clearInterval(timer);
+//       }
+//     }, delay);
+//   });
+// }
+
+// // fliednerDe()
+// export default fliednerDe;
