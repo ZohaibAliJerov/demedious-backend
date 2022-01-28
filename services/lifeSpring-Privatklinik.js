88d53f26e3e31824d4b89b9d32517ee5275bb3d4
adnan-5 <xetalishking333@gmail.com>
2022-01-28 19:28:30 +0500

    get all jobs details

--- services/lifeSpring-Privatklinik.js
+++ services/lifeSpring-Privatklinik.js
@@ -36,13 +36,79 @@ let LIFESPRING_Privatklinik = async () => {
     });
   await page.waitForTimeout(1000)
     console.log(jobLinks)
+    let allJobs = [];
+    for (let jobLink of jobLinks) {
+      let job = {
+        title: "",
+        location: "",
+        hospital: "LIFESPRING - Privatklinik Bad Münstereifel",
+        link: "",
+        level: "",
+        position: "",
+        city : "Bad Münstereifel",
+        email: "",
+        republic: "North Rhine-Westphalia",
+      };
 
+      await page.goto(jobLink, {
+        waitUntil: "load",
+        timeout: 0,
+      });
+      let title = await page.evaluate(() => {
+        let jbtitle = document.querySelector('div.col-sm-12.flex-intro-title > h1')
+        return jbtitle ? jbtitle.innerText : null;
+      });
+      job.title = title;
+
+      job.location = await page.evaluate(() => {
+        let loc = document.querySelector('#anschrift > p:nth-child(4)')
+      return loc  ? loc.innerText : null
+      });
+
+      let text = await page.evaluate(() => {
+        return document.body.innerText;
+      });
+      //get level
+      let level = text.match(/Facharzt|Chefarzt|Assistenzarzt|Arzt|Oberarzt/);
+      let position = text.match(/arzt|pflege/);
+      job.level = level ? level[0] : "";
+      if (
+        level == "Facharzt" ||
+        level == "Chefarzt" ||
+        level == "Assistenzarzt" ||
+        level == "Arzt" ||
+        level == "Oberarzt"
+      ) {
+        job.position = "artz";
+      }
+      if (position == "pflege" || (position == "Pflege" && !level in levels)) {
+        job.position = "pflege";
+        job.level = "Nicht angegeben";
+      }
+
+      if (!position in positions) {
+        continue;
+      }
+
+      //get link
+      job.email = await page.evaluate(() => {
+        return document.body.innerText.match(/[a-zA-Z0-9_+/.-]+@[a-zA-Z0-9.-]+\.[a-zA-Z0-9]+/g);
+      });
+      if (typeof job.email == "object"){
+        job.email = "" + job.email;
+      };
+    // apply Links 
+    job.link = jobLink;
+    await page.waitForTimeout(1000)
+      allJobs.push(job);
+    }
+    await page.waitForTimeout(3000)
+    console.log(allJobs);
     await browser.close()
-    await page.close()
+    // await page.close()
+    return allJobs.filter((job) => job.position != "");
 } catch (err) {
     console.error(err);
   }
 };
 
-
-LIFESPRING_Privatklinik()
