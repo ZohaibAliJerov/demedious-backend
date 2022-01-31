1e4e4ff9010d785bbc6f1d654f368809f1e63b06
sam <shoaibur206@gmail.com>
2022-01-31 19:40:49 +0500

    add condition to check, whether the site has jobs or not

--- services/fabricius.js
+++ services/fabricius.js
@@ -24,72 +24,76 @@ const fabricius = async () => {
       }
     );
   });
-  print(links);
-  //slice the links
-  //get all job details
-  let allJobs = [];
-  for (let link of links) {
-    await page.goto(link, { timeout: 0, waitUntil: "load" });
-    await page.waitForTimeout(5000);
-    let job = {
-      title: "",
-      location: "Köln",
-      hospital: "Sana Dreifaltigkeits-Krank",
-      link: "",
-      level: "",
-      position: "",
-      city: "Köln",
-      email: "",
-      republic: "North Rhine-Westphalia",
-    };
+  if (links.length > 0) {
+    print(links);
+    //slice the links
+    //get all job details
+    let allJobs = [];
+    for (let link of links) {
+      await page.goto(link, { timeout: 0, waitUntil: "load" });
+      await page.waitForTimeout(5000);
+      let job = {
+        title: "",
+        location: "Köln",
+        hospital: "Sana Dreifaltigkeits-Krank",
+        link: "",
+        level: "",
+        position: "",
+        city: "Köln",
+        email: "",
+        republic: "North Rhine-Westphalia",
+      };
 
-    await scroll(page);
-    job.title = await page.evaluate(() => {
-      return document.querySelector("h1").innerText;
-    });
-    job.email = await page.evaluate(() => {
-      return document.body.innerText.match(/\w+@\w+\.\w+/);
-    });
-    if (typeof job.email == "object") {
-      job.email = job.email[0];
-    }
-    job.location = await page.evaluate(() => {
-      return document.querySelector(".jobmeta > li").innerText;
-    });
-    let text = await page.evaluate(() => {
-      return document.body.innerText;
-    });
+      await scroll(page);
+      job.title = await page.evaluate(() => {
+        return document.querySelector("h1").innerText;
+      });
+      job.email = await page.evaluate(() => {
+        return document.body.innerText.match(/\w+@\w+\.\w+/);
+      });
+      if (typeof job.email == "object") {
+        job.email = job.email[0];
+      }
+      job.location = await page.evaluate(() => {
+        return document.querySelector(".jobmeta > li").innerText;
+      });
+      let text = await page.evaluate(() => {
+        return document.body.innerText;
+      });
 
-    //get level and positions
-    let level = text.match(/Facharzt|Chefarzt|Assistenzarzt|Arzt|Oberarzt/);
-    let position = text.match(/arzt|pflege/);
-    job.level = level ? level[0] : "";
-    if (
-      level == "Facharzt" ||
-      level == "Chefarzt" ||
-      level == "Assistenzarzt" ||
-      level == "Arzt" ||
-      level == "Oberarzt"
-    ) {
-      job.position = "artz";
-    }
-    if (position == "pflege" || (position == "Pflege" && !level in levels)) {
-      job.position = "pflege";
-      job.level = "Nicht angegeben";
-    }
+      //get level and positions
+      let level = text.match(/Facharzt|Chefarzt|Assistenzarzt|Arzt|Oberarzt/);
+      let position = text.match(/arzt|pflege/);
+      job.level = level ? level[0] : "";
+      if (
+        level == "Facharzt" ||
+        level == "Chefarzt" ||
+        level == "Assistenzarzt" ||
+        level == "Arzt" ||
+        level == "Oberarzt"
+      ) {
+        job.position = "artz";
+      }
+      if (position == "pflege" || (position == "Pflege" && !level in levels)) {
+        job.position = "pflege";
+        job.level = "Nicht angegeben";
+      }
 
-    if (!position in positions) {
-      continue;
-    }
-    job.link = link;
-    if (typeof job.link == "object") {
-      job.link = job.link[0];
-    }
-    allJobs.push(job);
-  } //end of for loop
-  await page.close();
-  await browser.close();
-  return allJobs;
+      if (!position in positions) {
+        continue;
+      }
+      job.link = link;
+      if (typeof job.link == "object") {
+        job.link = job.link[0];
+      }
+      allJobs.push(job);
+    } //end of for loop
+    await page.close();
+    await browser.close();
+    return allJobs;
+  } else {
+    return [];
+  }
 };
 
 async function scroll(page) {
