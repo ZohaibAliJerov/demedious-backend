5c8568049a5b5eada15ee03fe7ea0305e0f0881e
sam <shoaibur206@gmail.com>
2022-01-31 20:11:31 +0500

    modify function name

--- services/gerrieshiem.js
+++ services/gerrieshiem.js
@@ -3,7 +3,7 @@ import puppeteer from "puppeteer";
 let positions = ["arzt", "pflege"];
 let levels = ["Facharzt", "Chefarzt", "Assistenzarzt", "Arzt", "Oberarzt"];
 
-const bernath = async () => {
+const gerrieshiem = async () => {
   let browser = await puppeteer.launch({ headless: false });
   let page = await browser.newPage();
 
@@ -117,4 +117,4 @@ function print(...args) {
   console.log(...args);
 }
 
-export default bernath;
+export default gerrieshiem;
