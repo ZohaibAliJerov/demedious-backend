b52bf7e154c8a9b7e99bb62bb8a0b573298b081f
sam <shoaibur206@gmail.com>
2022-01-31 20:19:52 +0500

    modify site url

--- services/remschied.js
+++ services/remschied.js
@@ -7,8 +7,7 @@ const remschied = async () => {
   let browser = await puppeteer.launch({ headless: false });
   let page = await browser.newPage();
 
-  let url =
-    "https://www.sana.de/duesseldorf-gerresheim/karriere/stellenangebote/#c63039";
+  let url = "https://www.sana.de/remscheid/karriere/stellenangebote/#c63039";
 
   await page.goto(url, { timeout: 0, waitUntil: "load" });
 
