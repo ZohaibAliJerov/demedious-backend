8e1984edbbbea2785d58332a53b420e955ff0195
sam <shoaibur206@gmail.com>
2022-01-31 20:04:52 +0500

    modify function name

--- services/bernath.js
+++ services/bernath.js
@@ -3,7 +3,7 @@ import puppeteer from "puppeteer";
 let positions = ["arzt", "pflege"];
 let levels = ["Facharzt", "Chefarzt", "Assistenzarzt", "Arzt", "Oberarzt"];
 
-const duisburg = async () => {
+const bernath = async () => {
   let browser = await puppeteer.launch({ headless: false });
   let page = await browser.newPage();
 
@@ -117,4 +117,4 @@ function print(...args) {
   console.log(...args);
 }
 
-export default duisburg;
+export default bernath;
