8dc0d5164b38c6f732c2ac55f366af49f96a54fc
sher-karim05 <karimdeveloper12@gmail.com>
2022-02-02 19:02:39 +0500

    Add upadates

--- services/mainService.js
+++ services/mainService.js
@@ -3,20 +3,18 @@
 // import krakehan from "./krakenhaus.js";
 // import dusseldorf from "./dusseldorf.js";
 //import johanniter from "./johanniter.js";
-
-import haan from "./haan.js";
-import gut from "./gut.js";
-import gutersloh from "./gutersloh.js";
-import hoxter from "./hoxter.js";
-import langenfeld from "./langenfeld.js";
-import koln1 from "./koln1.js";
-import herten from "./herten.js";
-import hilden from "./hilden.js";
-import duren from "./duren.js";
-import dortmund1 from "./dortmund1.js";
-import dortmund2 from "./dortmund2.js";
-import dortmund3 from "./dortmund3.js";
-
+// import haan from "./haan.js";
+// import gut from "./gut.js";
+// import gutersloh from "./gutersloh.js";
+// import hoxter from "./hoxter.js";
+// import langenfeld from "./langenfeld.js";
+// import koln1 from "./koln1.js";
+// import herten from "./herten.js";
+// import hilden from "./hilden.js";
+// import duren from "./duren.js";
+// import dortmund1 from "./dortmund1.js";
+// import dortmund2 from "./dortmund2.js";
+// import dortmund3 from "./dortmund3.js";
 //import koln from "./koln.js";
 
 const mainService = async () => {
