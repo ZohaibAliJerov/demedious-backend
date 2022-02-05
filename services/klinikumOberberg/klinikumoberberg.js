a2ab6ad0c2ded7e576ebe76bc5fa585b01f3cd0b
sher-karim05 <karimdeveloper12@gmail.com>
2022-02-05 20:24:40 +0500

    Add updates

---
+++ services/klinikumOberberg/klinikumoberberg.js
@@ -0,0 +1,145 @@
+import puppeteer from "puppeteer";
+
+let positions = ["arzt", "pflege"];
+let levels = ["Facharzt", "Chefarzt", "Assistenzarzt", "Arzt", "Oberarzt"];
+
+let klinikumOberberg = async () => {
+  try {
+    let browser = await puppeteer.launch({
+      headless: false,
+    });
+    let page = await browser.newPage();
+
+    await page.goto(
+      "https://www.klinikum-oberberg.de/karriereausbildung/stellenangebote",
+      {
+        waitUntil: "load",
+        timeout: 0,
+      }
+    );
+
+    let nextPage = true;
+    let allJobLinks = [];
+    while (nextPage) {
+      //scroll the page
+      await scroll(page);
+
+      await page.waitForTimeout(1000);
+      //get all jobLinks
+      let jobLinks = await page.evaluate(() => {
+        return Array.from(
+          document.querySelectorAll(
+            "td.detailsLink.jsDetailsLink > a"
+          )
+        ).map((el) => el.href);
+      });
+      allJobLinks.push(...jobLinks);
+
+      await page.waitForTimeout(1000);
+      let bottomNextLink = await page.evaluate(() => {
+        return document.querySelector(
+          "div#esDatentabelle_paginate > ul > li.paginate_button.next > a"
+        );
+      });
+      if (bottomNextLink) {
+        await page.click(
+          "div#esDatentabelle_paginate > ul > li.paginate_button.next > a"
+        );
+        nextPage = true;
+      } else {
+        nextPage = false;
+      }
+    } //end of while loop
+
+    console.log(allJobLinks);
+
+    let allJobs = [];
+
+    for (let jobLink of allJobLinks) {
+      let job = {
+        title: "",
+        location: "Gummersbach",
+        hospital: "klinikum-oberberg",
+        city: "Gummersbach ",
+        link: "",
+        level: "",
+        email: "",
+        position: "",
+        republic: "North Rhine-Westphalia",
+      };
+      await page.goto(jobLink, {
+        waitUntil: "load",
+        timeout: 0,
+      });
+
+      await page.waitForTimeout(1000);
+      //get title
+      let title = await page.evaluate(() => {
+        let ttitle = document.querySelector("h1.obsInformation__stellenTitel");
+        return ttitle ? ttitle.innerText : "";
+      });
+      job.title = title;
+
+      let text = await page.evaluate(() => {
+        return document.body.innerText;
+      });
+      //get email
+      let email = await page.evaluate(() => {
+          let eml = document.querySelector("#c5071 > div > div > div.news-text-wrap > p:nth-child(10) > a");
+        return  eml ? eml.innerText : "N/A";
+      });
+      job.email = String() + email;
+      //apply link
+      let link = await page.evaluate(()=>{
+          let lnk = document.querySelector("a.btn.btn--akzent1.btn-lg.btn-block");
+          return lnk ? lnk.href : "N/A";
+      })
+      job.link = link;
+      //get level
+      let level = text.match(/Facharzt|Chefarzt|Assistenzarzt/);
+      let position = text.match(/arzt|pflege/);
+      job.level = level ? level[0] : "";
+      if (
+        level == "Facharzt" ||
+        level == "Chefarzt" ||
+        level == "Assistenzarzt" ||
+        level == "Arzt" ||
+        level == "Oberarzt"
+      ) {
+        job.position = "artz";
+      }
+      if (position == "pflege" || (position == "Pflege" && !level in levels)) {
+        job.position = "pflege";
+        job.level = "Nicht angegeben";
+      }
+
+      if (!position in positions) {
+        continue;
+      }
+
+      allJobs.push(job);
+    }
+    console.log(allJobs);
+    return allJobs.filter((job) => job.position != "");
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+async function scroll(page) {
+  await page.evaluate(() => {
+    const distance = 100;
+    const delay = 100;
+    const timer = setInterval(() => {
+      document.scrollingElement.scrollBy(0, distance);
+      if (
+        document.scrollingElement.scrollTop + window.innerHeight >=
+        document.scrollingElement.scrollHeight
+      ) {
+        clearInterval(timer);
+      }
+    }, delay);
+  });
+}
+klinikumOberberg();
+export default klinikumOberberg;
