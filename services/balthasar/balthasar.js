bb316ad6e6cb12cf48ad288aedee2344a8b1a9a1
sher-karim05 <karimdeveloper12@gmail.com>
2022-02-08 03:22:54 +0500

    Add updates in balthasar.js

---
+++ services/balthasar/balthasar.js
@@ -0,0 +1,124 @@
+import puppeteer from "puppeteer";
+
+let positions = ["arzt", "pflege"];
+let levels = ["Facharzt", "Chefarzt", "Assistenzarzt", "Arzt", "Oberarzt"];
+
+let balthasar = async () => {
+  try {
+    let browser = await puppeteer.launch({
+      headless: false,
+    });
+    let page = await browser.newPage();
+
+    await page.goto(
+      "https://www.kinderhospiz-balthasar.de/service/stellenangebote.html",
+      {
+        waitUntil: "load",
+        timeout: 0,
+      }
+    );
+      //scroll the page
+      await scroll(page);
+
+      await page.waitForTimeout(1000);
+      //get all jobLinks
+      let jobLinks = await page.evaluate(() => {
+        return Array.from(
+          document.querySelectorAll(
+            "div.job-item.shuffle-item.shuffle-item--visible > div > a"
+          )
+        ).map((el) => el.href);
+      });
+
+    console.log(jobLinks);
+
+    let allJobs = [];
+
+    for (let jobLink of jobLinks) {
+      let job = {
+        title: "",
+        location: "57462 Olpe ",
+        hospital: "balthasar- Kinder-und Jugendhospiz",
+        city: "Olpe",
+        link: "",
+        level: "",
+        email: "",
+        position: "",
+        republic: "North Rhine-Westphalia",
+      };
+      await page.goto(jobLink, {
+        waitUntil: "load",
+        timeout: 0,
+      });
+
+      await page.waitForTimeout(1000);
+      //get title
+      let title = await page.evaluate(() => {
+        let ttitle = document.querySelector("h1.text-center");
+        return ttitle ? ttitle.innerText : "";
+      });
+      job.title = title;
+
+      let text = await page.evaluate(() => {
+        return document.body.innerText;
+      });
+      //get email
+      let email = await page.evaluate(() => {
+          let eml = document.querySelector("#tx_smsjobboerse_tab0_inhalt > div > p:nth-child(18) > a");
+        return eml ? eml.innerText : "N/A";
+      });
+      job.email = String() + email;
+      //apply link
+      let link = await page.evaluate(()=>{
+          let lnk = document.querySelector("#tx_smsjobboerse_tabs_inhalt > div.inhaltbuttonrow > a.onlinebewerben.btn.btn--invert");
+          return lnk ? lnk.href : "N/A";
+      })
+      job.link = link;
+      //get level
+      let level = text.match(/Facharzt|Chefarzt|Assistenzarzt/);
+      let position = text.match(/arzt|pflege/);
+      job.level = level ? level[0] : "";
+      if (
+        level == "Facharzt" ||
+        level == "Chefarzt" ||
+        level == "Assistenzarzt" ||
+        level == "Arzt" ||
+        level == "Oberarzt"
+      ) {
+        job.position = "artz";
+      }
+      if (position == "pflege" || (position == "Pflege" && !level in levels)) {
+        job.position = "pflege";
+        job.level = "Nicht angegeben";
+      }
+
+      if (!position in positions) {
+        continue;
+      }
+
+      allJobs.push(job);
+    }
+    console.log(allJobs);
+    return allJobs.filter((job) => job.position != "");
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+async function scroll(page) {
+  await page.evaluate(() => {
+    const distance = 100;
+    const delay = 100;
+    const timer = setInterval(() => {
+      document.scrollingElement.scrollBy(0, distance);
+      if (
+        document.scrollingElement.scrollTop + window.innerHeight >=
+        document.scrollingElement.scrollHeight
+      ) {
+        clearInterval(timer);
+      }
+    }, delay);
+  });
+}
+balthasar();
+export default balthasar;
