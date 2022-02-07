4f88805943bcc00caf05fe69d7136b58283b1b5f
sher-karim05 <karimdeveloper12@gmail.com>
2022-02-08 01:57:48 +0500

    Add updates

---
+++ services/jonniterThree/jonniterThree.js
@@ -0,0 +1,124 @@
+import puppeteer from "puppeteer";
+
+let positions = ["arzt", "pflege"];
+let levels = ["Facharzt", "Chefarzt", "Assistenzarzt", "Arzt", "Oberarzt"];
+
+let johnniterThree = async () => {
+  try {
+    let browser = await puppeteer.launch({
+      headless: false,
+    });
+    let page = await browser.newPage();
+
+    await page.goto(
+      "https://www.johanniter.de/johanniter-kliniken/kinderfachklinik-bad-sassendorf/karriere/",
+      {
+        waitUntil: "load",
+        timeout: 0,
+      }
+    );
+      //scroll the page
+      await scroll(page);
+
+      await page.waitForTimeout(1000);
+      //get all jobLinks
+      let jobLinks = await page.evaluate(() => {
+        return Array.from(
+          document.querySelectorAll(
+            "div.c-content-list__text > h3 > a"
+          )
+        ).map((el) => el.href);
+      });
+
+    console.log(jobLinks);
+
+    let allJobs = [];
+
+    for (let jobLink of jobLinks) {
+      let job = {
+        title: "",
+        location: "Bad Sassendorf",
+        hospital: "johanniter",
+        city: "Bad Sassendorf ",
+        link: "",
+        level: "",
+        email: "",
+        position: "",
+        republic: "North Rhine-Westphalia",
+      };
+      await page.goto(jobLink, {
+        waitUntil: "load",
+        timeout: 0,
+      });
+
+      await page.waitForTimeout(1000);
+      //get title
+      let title = await page.evaluate(() => {
+        let ttitle = document.querySelector("div.c-page-title > h1");
+        return ttitle ? ttitle.innerText : "";
+      });
+      job.title = title;
+
+      let text = await page.evaluate(() => {
+        return document.body.innerText;
+      });
+      //get email
+      let email = await page.evaluate(() => {
+          let eml = document.querySelector("#main-content > div:nth-child(4) > p:nth-child(3) > a");
+        return eml ? eml.innerText : "N/A";
+      });
+      job.email = String() + email;
+      //apply link
+      let link = await page.evaluate(()=>{
+          let lnk = document.querySelector("a.c-button.c-button--main.c-button--large");
+          return lnk ? lnk.href : "N/A";
+      })
+      job.link = link;
+      //get level
+      let level = text.match(/Facharzt|Chefarzt|Assistenzarzt/);
+      let position = text.match(/arzt|pflege/);
+      job.level = level ? level[0] : "";
+      if (
+        level == "Facharzt" ||
+        level == "Chefarzt" ||
+        level == "Assistenzarzt" ||
+        level == "Arzt" ||
+        level == "Oberarzt"
+      ) {
+        job.position = "artz";
+      }
+      if (position == "pflege" || (position == "Pflege" && !level in levels)) {
+        job.position = "pflege";
+        job.level = "Nicht angegeben";
+      }
+
+      if (!position in positions) {
+        continue;
+      }
+
+      allJobs.push(job);
+    }
+    console.log(allJobs);
+    return allJobs.filter((job) => job.position != "");
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+async function scroll(page) {
+  await page.evaluate(() => {
+    const distance = 100;
+    const delay = 100;
+    const timer = setInterval(() => {
+      document.scrollingElement.scrollBy(0, distance);
+      if (
+        document.scrollingElement.scrollTop + window.innerHeight >=
+        document.scrollingElement.scrollHeight
+      ) {
+        clearInterval(timer);
+      }
+    }, delay);
+  });
+}
+johnniterThree();
+export default johnniterThree;
