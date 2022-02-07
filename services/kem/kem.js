3f8c333d9c3bd563c05b956f81bd6e50cb7b24a6
sher-karim05 <karimdeveloper12@gmail.com>
2022-02-08 01:49:32 +0500

    Add updates

---
+++ services/kem/kem.js
@@ -0,0 +1,119 @@
+import puppeteer from "puppeteer";
+
+let positions = ["arzt", "pflege"];
+let levels = ["Facharzt", "Chefarzt", "Assistenzarzt", "Arzt", "Oberarzt"];
+
+let kem = async () => {
+  try {
+    let browser = await puppeteer.launch({
+      headless: false,
+    });
+    let page = await browser.newPage();
+
+    await page.goto(
+      "https://kem-med.com/ausbildung-und-karriere/karriere/stellenmarkt/pflege-funktionsdienst/",
+      {
+        waitUntil: "load",
+        timeout: 0,
+      }
+    );
+      //get all jobLinks
+      let jobLinks = await page.evaluate(() => {
+        return Array.from(
+          document.querySelectorAll(
+            "div.bite_main > p > a"
+          )
+        ).map((el) => el.href);
+      });
+     
+    console.log(jobLinks);
+
+    let allJobs = [];
+
+    for (let jobLink of jobLinks) {
+      let job = {
+        title: "",
+        location: "44263 Dortmund",
+        hospital: "Kath St.Paulus Gesellschaft",
+        city: "Dortmund",
+        link: "",
+        level: "",
+        email: "",
+        position: "",
+        republic: "North Rhine-Westphalia",
+      };
+      await page.goto(jobLink, {
+        waitUntil: "load",
+        timeout: 0,
+      });
+
+      await page.waitForTimeout(1000);
+      //get title
+      let title = await page.evaluate(() => {
+        let ttitle = document.querySelector("div.__bjp-content--block __bjp-border-section > h2");
+        return ttitle ? ttitle.innerText : "";
+      });
+      job.title = title;
+
+      let text = await page.evaluate(() => {
+        return document.body.innerText;
+      });
+      //get email
+      let email = await page.evaluate(() => {
+        return document.body.innerText.match(/\w+\@\w+.\w+\-\w+.\w+/) || "N/A";
+      });
+      job.email = String() + email;
+      //apply link
+      let link = await page.evaluate(()=>{
+          let lnk = document.querySelector("a.__bjp-btn");
+          return lnk ? lnk.href : "N/A";
+      })
+      job.link = link;
+      //get level
+      let level = text.match(/Facharzt|Chefarzt|Assistenzarzt/);
+      let position = text.match(/arzt|pflege/);
+      job.level = level ? level[0] : "";
+      if (
+        level == "Facharzt" ||
+        level == "Chefarzt" ||
+        level == "Assistenzarzt" ||
+        level == "Arzt" ||
+        level == "Oberarzt"
+      ) {
+        job.position = "artz";
+      }
+      if (position == "pflege" || (position == "Pflege" && !level in levels)) {
+        job.position = "pflege";
+        job.level = "Nicht angegeben";
+      }
+
+      if (!position in positions) {
+        continue;
+      }
+
+      allJobs.push(job);
+    }
+    console.log(allJobs);
+    return allJobs.filter((job) => job.position != "");
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+async function scroll(page) {
+  await page.evaluate(() => {
+    const distance = 100;
+    const delay = 100;
+    const timer = setInterval(() => {
+      document.scrollingElement.scrollBy(0, distance);
+      if (
+        document.scrollingElement.scrollTop + window.innerHeight >=
+        document.scrollingElement.scrollHeight
+      ) {
+        clearInterval(timer);
+      }
+    }, delay);
+  });
+}
+kem();
+export default kem;
